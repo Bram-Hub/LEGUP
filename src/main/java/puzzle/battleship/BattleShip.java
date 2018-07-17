172c81cd61381a8af58ddeedcd925e8c6a83f73e
Jeffrey Poegel <jpoegel@comcast.net>
2018-07-17 17:45:15 -0400

    Fixed Battleship errors

--- src/main/java/puzzle/battleship/BattleShip.java
+++ src/main/java/puzzle/battleship/BattleShip.java
@@ -6,13 +6,11 @@ import model.gameboard.ElementData;
 import model.tree.Tree;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import ui.boardview.PuzzleElement;
 
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.io.FileInputStream;
@@ -74,5 +72,83 @@ public class BattleShip extends Puzzle
     public void onBoardChange(Board board)
     {
 
+    }
+//
+//    @Override
+//    public void onTreeSelectionChange(ArrayList<Selection> newSelection)
+//    {
+//
+//    }
+
+    @Override
+    public void importPuzzle(String fileName)
+    {
+//        if(fileName != null)
+//        {
+//            InputStream inputStream = new FileInputStream(fileName);
+//            DocumentBuilder builder = null;//factory.newDocumentBuilder();
+//            Document document = builder.parse(inputStream);
+//
+//            BattleShipBoard battleShipBoard;
+//
+//            Element rootNode = document.getDocumentElement();
+//            Element puzzleElement = (Element) rootNode.getElementsByTagName("puzzle").item(0);
+//            Element boardElement = (Element) puzzleElement.getElementsByTagName("board").item(0);
+//            Element axesElement = (Element) boardElement.getElementsByTagName("axes").item(0);
+//            Element shipElement = (Element) boardElement.getElementsByTagName("ships").item(0);
+//            Element cellElement = (Element) boardElement.getElementsByTagName("cells").item(0);
+//            Element rightElement = (Element) axesElement.getElementsByTagName("right").item(0);
+//            Element bottomElement = (Element) axesElement.getElementsByTagName("bottom").item(0);
+//            NodeList rightClueList = rightElement.getElementsByTagName("clue");
+//            NodeList bottomClueList = bottomElement.getElementsByTagName("clue");
+//            NodeList shipList = shipElement.getElementsByTagName("ship");
+//            NodeList cells = cellElement.getElementsByTagName("cell");
+//
+//            int size = Integer.valueOf(boardElement.getAttribute("size"));
+//            battleShipBoard = new BattleShipBoard(size);
+//
+//            ArrayList<ElementData> battleShipData = new ArrayList<>();
+//            for(int i = 0; i < size * size; i++)
+//            {
+//                battleShipData.add(null);
+//            }
+//
+//            for (int i = 0; i < rightClueList.getLength(); i++) {
+//                battleShipBoard.getRight()[i] = Integer.valueOf(rightClueList.item(i).getAttributes().getNamedItem("value").getNodeValue());
+//            }
+//
+//            for (int i = 0; i < bottomClueList.getLength(); i++) {
+//                battleShipBoard.getBottom()[i] = Integer.valueOf(bottomClueList.item(i).getAttributes().getNamedItem("value").getNodeValue());
+//            }
+//
+//            for (int i = 0; i < shipList.getLength(); i++) {
+//                int length = Integer.valueOf(shipList.item(i).getAttributes().getNamedItem("length").getNodeValue());
+//                int count = Integer.valueOf(shipList.item(i).getAttributes().getNamedItem("count").getNodeValue());
+//                battleShipBoard.getShips().add(new Ship(length, count));
+//            }
+//
+//            for (int i = 0; i < cells.getLength(); i++) {
+//                int x = Integer.valueOf(cells.item(i).getAttributes().getNamedItem("x").getNodeValue());
+//                int y = Integer.valueOf(cells.item(i).getAttributes().getNamedItem("y").getNodeValue());
+//                String value = cells.item(i).getAttributes().getNamedItem("value").getNodeValue().toUpperCase();
+//
+//                BattleShipCell cell = new BattleShipCell(BattleShipCellType.valueOf(value).ordinal(), new Point(x, y));
+//                battleShipBoard.setCell(x, y, cell);
+//                cell.setModifiable(false);
+//                cell.setGiven(true);
+//            }
+//
+//            for (int x = 0; x < size; x++) {
+//                for (int y = 0; y < size; y++) {
+//                    if (battleShipBoard.getCell(x, y) == null) {
+//                        BattleShipCell cell = new BattleShipCell(9, new Point(x, y));
+//                        cell.setModifiable(true);
+//                        battleShipBoard.setCell(x, y, cell);
+//                    }
+//                }
+//            }
+//            this.currentBoard = battleShipBoard;
+//            this.tree = new Tree(currentBoard);
+//        }
     }
 }
