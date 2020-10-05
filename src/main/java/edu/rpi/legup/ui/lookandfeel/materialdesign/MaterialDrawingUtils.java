32f2fb8f36e1d0952969cf39c09d6f4c6ae61694
Benjamin Sherman <benjamin@bensherman.io>
2020-10-05 19:10:19 -0400

    Check for null pointer in getAliasedGraphics()

--- src/main/java/edu/rpi/legup/ui/lookandfeel/materialdesign/MaterialDrawingUtils.java
+++ src/main/java/edu/rpi/legup/ui/lookandfeel/materialdesign/MaterialDrawingUtils.java
@@ -16,12 +16,17 @@ public class MaterialDrawingUtils {
 
     public static Graphics getAliasedGraphics (Graphics g) {
         Map<RenderingHints.Key, Object> hints = (Map<RenderingHints.Key, Object>) Toolkit.getDefaultToolkit ().getDesktopProperty ("awt.font.desktophints");
-        hints.put (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if (hints != null) {
+          hints.put (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.addRenderingHints (hints);
+          Graphics2D g2d = (Graphics2D) g;
+          g2d.addRenderingHints (hints);
 
-        //g2d.addRenderingHints (new RenderingHints (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
-        return g2d;
+          //g2d.addRenderingHints (new RenderingHints (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
+          return g2d;
+        } else {
+          // Desktop hints not supported on this platform
+          return g;
+        }
     }
 }
