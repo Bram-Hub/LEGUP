322d7d8f4d0236509cab25ffee0068926ecbf4f4
Alapocas <sissijian0720@163.com>
2020-10-25 22:11:20 -0400

    Revert "Sudoku cell control and display"
    This reverts commit f7383d56286f97166a3802b7cefe696e4c7acf2b.

--- src/main/java/edu/rpi/legup/puzzle/sudoku/SudokuCellController.java
+++ src/main/java/edu/rpi/legup/puzzle/sudoku/SudokuCellController.java
@@ -1,42 +1,64 @@
 package edu.rpi.legup.puzzle.sudoku;
 
-/*import java.awt.event.MouseEvent;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import edu.rpi.legup.model.gameboard.PuzzleElement;
-import edu.rpi.legup.puzzle.treetent.TreeTentCell;*/
+public class SudokuCellController implements MouseListener {
+    private SudokuView sudokuView;
 
-import edu.rpi.legup.controller.ElementController;
-import edu.rpi.legup.model.gameboard.PuzzleElement;
+    public SudokuCellController(SudokuView sudokuView) {
+        this.sudokuView = sudokuView;
+    }
 
-import java.awt.event.MouseEvent;
-
-public class SudokuCellController extends ElementController {
+    /**
+     * Invoked when the mouse button has been clicked (pressed
+     * and released) on a component.
+     *
+     * @param e the event to be processed
+     */
     @Override
-    public void changeCell(MouseEvent e, PuzzleElement data) {
-        SudokuCell cell = (SudokuCell) data;
-        System.out.print(111);
-        if (e.getButton() == MouseEvent.BUTTON1) {
-            if (e.isControlDown()) {
-                this.boardView.getSelectionPopupMenu().show(boardView, this.boardView.getCanvas().getX() + e.getX(), this.boardView.getCanvas().getY() + e.getY());
-            } else {
-                if (cell.getData() == -2) {
-                    data.setData(-4);
-                } else if (cell.getData() == -4) {
-                    data.setData(-3);
-                } else {
-                    data.setData(-2);
-                }
-            }
-        } else if (e.getButton() == MouseEvent.BUTTON3) {
-            if (cell.getData() == -4) {
-                data.setData(-2);
-            } else if (cell.getData() == -2) {
-                data.setData(-3);
-            } else {
-                data.setData(-4);
-            }
-        }
+    public void mouseClicked(MouseEvent e) {
+        SudokuElementView cell = (SudokuElementView) e.getSource();
+        cell.getPuzzleElement().setData(cell.getPuzzleElement().getData() + 1);
+    }
+
+    /**
+     * Invoked when a mouse button has been pressed on a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when a mouse button has been released on a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when the mouse enters a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when the mouse exits a component.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
-
