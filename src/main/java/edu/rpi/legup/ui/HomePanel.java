5c1c10cc8d31063c5c65085e408e76f91eb2e9a2
Matt <matthew.sweet417@gmail.com>
2022-06-10 14:48:40 -0400

    Edited Home Panel to display image buttons

--- src/main/java/edu/rpi/legup/ui/HomePanel.java
+++ src/main/java/edu/rpi/legup/ui/HomePanel.java
@@ -8,11 +8,9 @@ public class HomePanel extends LegupPanel {
 
     private JButton[] buttons;
 
-    private JPanel buttonPanel;
-
     public HomePanel(LegupUI legupUI) {
         this.legupUI = legupUI;
-        setLayout(new GridLayout(2, 1));
+        setLayout(new GridLayout(1, 2));
         initButtons();
     }
 
@@ -23,18 +21,20 @@ public class HomePanel extends LegupPanel {
 
     private void initButtons() {
         this.buttons = new JButton[2];
-        this.buttons[0] = new JButton("Solve a puzzle!");
+        this.buttons[0] = new JButton("Open Proof");
+        this.buttons[0].setIcon(new ImageIcon("src/main/resources/edu/rpi/legup/homepanel/openproof.png"));
+        this.buttons[0].setHorizontalTextPosition(AbstractButton.CENTER);
+        this.buttons[0].setVerticalTextPosition(AbstractButton.BOTTOM);
         this.buttons[0].addActionListener(l -> this.legupUI.displayPanel(1));
-        this.buttons[1] = new JButton("Create/Edit a puzzle!");
+        this.buttons[1] = new JButton("Create/Edit Puzzle");
+        this.buttons[1].setIcon(new ImageIcon("src/main/resources/edu/rpi/legup/homepanel/edit.png"));
+        this.buttons[1].setHorizontalTextPosition(AbstractButton.CENTER);
+        this.buttons[1].setVerticalTextPosition(AbstractButton.BOTTOM);
         this.buttons[1].addActionListener(l -> this.legupUI.displayPanel(2));
-        this.buttonPanel = new JPanel();
-        for (JButton button : this.buttons) {
-            this.buttonPanel.add(button);
-        }
     }
 
     private void render() {
-        add(new JLabel("Welcome to LEGUP! What would you like to do?"));
-        add(this.buttonPanel);
+        add(this.buttons[0]);
+        add(this.buttons[1]);
     }
 }
