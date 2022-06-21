c29af43eff62b86bbe66c71f6049b0ca49058c63
Ivan Ho <41582274+Corppet@users.noreply.github.com>
2022-06-21 15:20:17 -0400

    JavaDoc comments for some board class methods. Also added more methods for identifying other cells in `BattleShipBoard`.

--- src/main/java/edu/rpi/legup/puzzle/battleship/BattleShipBoard.java
+++ src/main/java/edu/rpi/legup/puzzle/battleship/BattleShipBoard.java
@@ -2,17 +2,26 @@ package edu.rpi.legup.puzzle.battleship;
 
 import edu.rpi.legup.model.gameboard.GridBoard;
 import edu.rpi.legup.model.gameboard.PuzzleElement;
+import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BattleShipBoard extends GridBoard {
 
+    /**
+     * The column of clues on the east side of the board.
+     */
     private List<BattleShipClue> east;
+
+    /**
+     * The column of clues on the south side of the board.
+     */
     private List<BattleShipClue> south;
 
     /**
-     * BattleShipBoard Constructor creates a new battleship board from the specified width and height
+     * Constructor for creating a rectangular battleship board.
      *
      * @param width  width of the board
      * @param height height of the board
@@ -32,7 +41,7 @@ public class BattleShipBoard extends GridBoard {
     }
 
     /**
-     * BattleShipBoard Constructor creates a new battleship board which is square from the specified size
+     * Constructor for creating a square-sized battleship board.
      *
      * @param size size of the board
      */
@@ -43,7 +52,8 @@ public class BattleShipBoard extends GridBoard {
     /**
      * Gets the east {@link BattleShipClue}
      *
-     * @return east battle ship clues
+     * @return  List of <code>BattleShipClue</code> objects on the east
+     *          side of the board
      */
     public List<BattleShipClue> getEast() {
         return east;
@@ -65,7 +75,8 @@ public class BattleShipBoard extends GridBoard {
 
     @Override
     public BattleShipBoard copy() {
-        BattleShipBoard copy = new BattleShipBoard(dimension.width, dimension.height);
+        BattleShipBoard copy = new BattleShipBoard(dimension.width,
+                dimension.height);
         for (int x = 0; x < this.dimension.width; x++) {
             for (int y = 0; y < this.dimension.height; y++) {
                 copy.setCell(x, y, getCell(x, y).copy());
@@ -77,5 +88,75 @@ public class BattleShipBoard extends GridBoard {
         copy.east = this.east;
         copy.south = this.south;
         return copy;
+    }
+
+    /**
+     * Get a list of all orthogonally adjacent cells.
+     * 
+     * @param cell  The cell to get adjacent cells from.
+     * @return      List of adjacent cells in clockwise order:
+     *              <code>{ up, right, down, left }</code>
+     */
+    public List<BattleShipCell> getAdjacent(@NotNull BattleShipCell cell) {
+        List<BattleShipCell> adj = new ArrayList<>();
+        Point loc = cell.getLocation();
+        BattleShipCell up = getCell(loc.x, loc.y - 1);
+        BattleShipCell right = getCell(loc.x + 1, loc.y);
+        BattleShipCell down = getCell(loc.x, loc.y + 1);
+        BattleShipCell left = getCell(loc.x - 1, loc.y);
+        adj.add(up);
+        adj.add(right);
+        adj.add(down);
+        adj.add(left);
+        return adj;
+    }
+
+    /**
+     * Get a list of all diagonally adjacent cells.
+     *
+     * @param cell  The cell to get diagonally adjacent cells from.
+     * @return      List of diagonally adjacent cells in clockwise order:
+     *              <code>{ upRight, downRight, downLeft, upLeft }</code>
+     */
+    public List<BattleShipCell> getAdjDiagonals(@NotNull BattleShipCell cell) {
+        List<BattleShipCell> dia = new ArrayList<>();
+        Point loc = cell.getLocation();
+        BattleShipCell upRight = getCell(loc.x + 1, loc.y - 1);
+        BattleShipCell downRight = getCell(loc.x + 1, loc.y + 1);
+        BattleShipCell downLeft = getCell(loc.x - 1, loc.y + 1);
+        BattleShipCell upLeft = getCell(loc.x - 1, loc.y - 1);
+        dia.add(upRight);
+        dia.add(downRight);
+        dia.add(downLeft);
+        dia.add(upLeft);
+        return dia;
+    }
+
+    /**
+     * Get a list of cells in a row.
+     *
+     * @param y The y-coordinate of the row.
+     * @return  List of cells in the row in increasing x-coordinate order.
+     */
+    public List<BattleShipCell> getRow(int y) {
+        List<BattleShipCell> row = new ArrayList<>();
+        for (int x = 0; x < dimension.width; x++) {
+            row.add(getCell(x, y));
+        }
+        return row;
+    }
+
+    /**
+     * Get a list of cells in a column.
+     *
+     * @param x The x-coordinate of the column.
+     * @return  List of cells in the column in increasing y-coordinate order.
+     */
+    public List<BattleShipCell> getColumn(int x) {
+        List<BattleShipCell> column = new ArrayList<>();
+        for (int y = 0; y < dimension.height; y++) {
+            column.add(getCell(x, y));
+        }
+        return column;
     }
 }
--- src/main/java/edu/rpi/legup/puzzle/battleship/rules/AdjacentShipsContradictionRule.java
+++ src/main/java/edu/rpi/legup/puzzle/battleship/rules/AdjacentShipsContradictionRule.java
@@ -3,26 +3,46 @@ package edu.rpi.legup.puzzle.battleship.rules;
 import edu.rpi.legup.model.gameboard.Board;
 import edu.rpi.legup.model.gameboard.PuzzleElement;
 import edu.rpi.legup.model.rules.ContradictionRule;
+import edu.rpi.legup.puzzle.battleship.BattleShipBoard;
+import edu.rpi.legup.puzzle.battleship.BattleShipCell;
+import edu.rpi.legup.puzzle.battleship.BattleShipType;
+
+import java.util.List;
 
 public class AdjacentShipsContradictionRule extends ContradictionRule {
+
+    private final String NO_CONTRADICTION_MESSAGE
+            = "No instance of the contradiction " + this.ruleName + " here";
 
     public AdjacentShipsContradictionRule() {
         super("BTSP-CONT-0001",
                 "Adjacent Ships",
-                "",
-                "edu/rpi/legup/images/battleship/contradictions/AdjacentShips.png");
+                "Cells next to the battleship must be water.",
+                "edu/rpi/legup/images/battleship/contradictions" +
+                        "/AdjacentShips.png");
     }
 
     /**
-     * Checks whether the transition has a contradiction at the specific puzzleElement index using this rule
+     * Checks whether the transition has a contradiction at the specific
+     * {@link PuzzleElement} index using this rule
      *
      * @param board         board to check contradiction
-     * @param puzzleElement equivalent puzzleElement
-     * @return null if the transition contains a contradiction at the specified puzzleElement,
-     * otherwise error message
+     * @param puzzleElement equivalent {@link PuzzleElement}
+     * @return              <code>null</code> if the transition contains a
+     *                      contradiction at the specified {@link PuzzleElement},
+     *                      otherwise error message
      */
     @Override
     public String checkContradictionAt(Board board, PuzzleElement puzzleElement) {
+        BattleShipBoard bsBoard = (BattleShipBoard) board;
+        BattleShipCell cell = (BattleShipCell) bsBoard.getPuzzleElement(puzzleElement);
+
+        // check orthogonally adjacent cells
+        List<BattleShipCell> orthoAdjCells
+                = bsBoard.getAdjacent(cell);
+
+        BattleShipCell up = orthoAdjCells.get(0);
+
         return null;
     }
 }
