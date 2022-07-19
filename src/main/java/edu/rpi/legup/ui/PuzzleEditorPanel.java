9dd38190cc76064976a657288af3bec52d0aa438
Matthew Sweet <98288154+mattsweet417@users.noreply.github.com>
2022-07-19 15:17:19 -0400

    Nurikabe empty puzzles are now implemented

--- src/main/java/edu/rpi/legup/ui/PuzzleEditorPanel.java
+++ src/main/java/edu/rpi/legup/ui/PuzzleEditorPanel.java
@@ -192,6 +192,7 @@ public class PuzzleEditorPanel extends LegupPanel implements IHistoryListener {
             throw new IllegalArgumentException(exception.getMessage());
         }
         catch (RuntimeException e){
+            e.printStackTrace();
             LOGGER.error(e.getMessage());
         }
     }
