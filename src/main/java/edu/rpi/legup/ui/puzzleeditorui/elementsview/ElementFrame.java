a0bd9c44f9def03966b59d1773ac839ae3119236
HenryCullom <54919530+HenryCullom@users.noreply.github.com>
2022-08-05 14:22:32 -0400

    changed order of tabbed element panels put non-placeable elements first

--- src/main/java/edu/rpi/legup/ui/puzzleeditorui/elementsview/ElementFrame.java
+++ src/main/java/edu/rpi/legup/ui/puzzleeditorui/elementsview/ElementFrame.java
@@ -28,11 +28,11 @@ public class ElementFrame extends JPanel {
         this.status = new JLabel("", SwingConstants.CENTER);
         this.buttonGroup = new ButtonGroup();
 
-        placeableElementPanel = new PlaceableElementPanel(this);
-        tabbedPane.addTab(placeableElementPanel.getName(), placeableElementPanel.getIcon(), new JScrollPane(placeableElementPanel), placeableElementPanel.getToolTip());
-
         nonPlaceableElementPanel = new NonPlaceableElementPanel(this);
         tabbedPane.addTab(nonPlaceableElementPanel.getName(), nonPlaceableElementPanel.getIcon(), new JScrollPane(nonPlaceableElementPanel), nonPlaceableElementPanel.getToolTip());
+
+        placeableElementPanel = new PlaceableElementPanel(this);
+        tabbedPane.addTab(placeableElementPanel.getName(), placeableElementPanel.getIcon(), new JScrollPane(placeableElementPanel), placeableElementPanel.getToolTip());
 
         setLayout(new BorderLayout());
         setMinimumSize(new Dimension(250, 256));
@@ -56,8 +56,9 @@ public class ElementFrame extends JPanel {
     }
 
     public void setElements(Puzzle puzzle) {
-        placeableElementPanel.setElements(puzzle.getPlaceableElements());
         nonPlaceableElementPanel.setElements(puzzle.getNonPlaceableElements());
+        placeableElementPanel.setElements(puzzle.getPlaceableElements());
+
     }
 
     public EditorElementController getController() {
