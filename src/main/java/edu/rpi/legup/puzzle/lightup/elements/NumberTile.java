9cd981951674064b1583c76d6c943610b9afaacb
Felix <huj15@rpi.edu>
2022-10-31 23:33:58 -0400

    fixed issue 357

--- src/main/java/edu/rpi/legup/puzzle/lightup/elements/NumberTile.java
+++ src/main/java/edu/rpi/legup/puzzle/lightup/elements/NumberTile.java
@@ -6,6 +6,10 @@ public class NumberTile extends NonPlaceableElement {
 
     int object_number;
 
+    public NumberTile() {
+        super("LTUP-UNPL-0001", "Number Tile", "The number tile", "edu/rpi/legup/images/lightup/1.gif");
+    }
+    
     public NumberTile(int num) {
         super("LTUP-UNPL-0001", "Number Tile", "The number tile", "edu/rpi/legup/images/lightup/" + num + ".gif");
         if (num > 3 || num < 1) num = 1;
