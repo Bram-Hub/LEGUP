6a63b45e12e899abddaf162f5078a82a00504da4
Weijun Li <83390795+wei-jun7@users.noreply.github.com>
2022-11-01 16:33:15 -0400

    fix check style

--- src/main/java/edu/rpi/legup/ui/HomePanel.java
+++ src/main/java/edu/rpi/legup/ui/HomePanel.java
@@ -19,7 +19,7 @@ import java.util.Objects;
 
 import edu.rpi.legup.model.Puzzle;
 import edu.rpi.legup.save.InvalidFileFormatException;
-import jdk.javadoc.internal.tool.Main;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.xml.sax.Attributes;
