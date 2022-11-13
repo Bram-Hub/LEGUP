4b7463777dfda515f21afd735ad16b1043e8355a
Weijun Li <83390795+wei-jun7@users.noreply.github.com>
2022-11-12 21:46:31 -0500

    switch the bulb tile to NonPlaceable element

--- src/main/java/edu/rpi/legup/puzzle/lightup/elements/BulbTile.java
+++ src/main/java/edu/rpi/legup/puzzle/lightup/elements/BulbTile.java
@@ -1,8 +1,8 @@
 package edu.rpi.legup.puzzle.lightup.elements;
 
-import edu.rpi.legup.model.elements.PlaceableElement;
+import edu.rpi.legup.model.elements.NonPlaceableElement;
 
-public class BulbTile extends PlaceableElement {
+public class BulbTile extends NonPlaceableElement {
     public BulbTile() {
         super("LTUP-PLAC-0001", "Bulb Tile", "The bulb tile", "edu/rpi/legup/images/lightup/light.png");
     }
