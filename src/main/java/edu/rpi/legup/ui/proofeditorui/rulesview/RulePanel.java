7ba78471b72029d2b42c43b455b9750b6cd55af8
CharSong24 <songh5@rpi.edu>
2022-12-05 21:38:57 -0500

    1

--- src/main/java/edu/rpi/legup/ui/proofeditorui/rulesview/RulePanel.java
+++ src/main/java/edu/rpi/legup/ui/proofeditorui/rulesview/RulePanel.java
@@ -139,7 +139,9 @@ public abstract class RulePanel extends JPanel {
             longer = s2; shorter = s1;
         }
         int longerLength = longer.length();
-        if (longerLength == 0) { return 1.0; /* both strings are zero length */ }
+        if (longerLength == 0) {
+            return 1.0; /* both strings are zero length */
+        }
         return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
     }
     /**
@@ -153,21 +155,23 @@ public abstract class RulePanel extends JPanel {
         for (int i = 0; i <= s1.length(); i++) {
             int lastValue = i;
             for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0)
+                if (i == 0) {
                     costs[j] = j;
+                }
                 else {
                     if (j > 0) {
                         int newValue = costs[j - 1];
-                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                            newValue = Math.min(Math.min(newValue, lastValue),
-                                    costs[j]) + 1;
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        }
                         costs[j - 1] = lastValue;
                         lastValue = newValue;
                     }
                 }
             }
-            if (i > 0)
+            if (i > 0) {
                 costs[s2.length()] = lastValue;
+            }
         }
         return costs[s2.length()];
     }
