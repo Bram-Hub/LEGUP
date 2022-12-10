e0226b93227d76d40f7e8d7a37cf8f276efe805d
CharSong24 <songh5@rpi.edu>
2022-12-09 23:25:21 -0500

    add comments

--- src/main/java/edu/rpi/legup/ui/proofeditorui/rulesview/RulePanel.java
+++ src/main/java/edu/rpi/legup/ui/proofeditorui/rulesview/RulePanel.java
@@ -132,6 +132,11 @@ public abstract class RulePanel extends JPanel {
 
     /**
      * Calculates the similarity (a number within 0 and 1) between two strings.
+     * This funtion will take two para String s1 and String s2, which s1 is the user's input
+     * and s2 is the compared really rule name
+     *
+     * similarityCheck will use a helper function to calculate a similarity degree(from 0 to 1).
+     * closer to 0 means less similar, and closer to 1 means more similar.
      */
     public static double similarityCheck(String s1, String s2) {
         String longer = s1, shorter = s2;
