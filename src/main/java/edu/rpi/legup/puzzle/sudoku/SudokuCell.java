925672210f8d43a01ca196f1781e3e2688a9c3ea
Jun <song.junyoung64@gmail.com>
2023-02-07 17:24:19 -0500

    removed an import line in SudokuCell that was unnecessarily added

--- src/main/java/edu/rpi/legup/puzzle/sudoku/SudokuCell.java
+++ src/main/java/edu/rpi/legup/puzzle/sudoku/SudokuCell.java
@@ -1,7 +1,6 @@
 package edu.rpi.legup.puzzle.sudoku;
 
 import edu.rpi.legup.model.gameboard.GridCell;
-import edu.rpi.legup.puzzle.sudoku.elements.NumberTile;
 
 import java.awt.*;
 import java.util.HashSet;
