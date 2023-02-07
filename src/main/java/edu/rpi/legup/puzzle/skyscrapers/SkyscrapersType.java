f96486d768de838e88df1ad49e2e31c87d35019f
Jun <song.junyoung64@gmail.com>
2023-02-07 17:18:28 -0500

    changed SkyscrapersCell class to use its own respective class type, SkyScrapersType instead of int. modified the files initializing/utilizing SkyscrapersCell to use SkyScrapersType.

--- src/main/java/edu/rpi/legup/puzzle/skyscrapers/SkyscrapersType.java
+++ src/main/java/edu/rpi/legup/puzzle/skyscrapers/SkyscrapersType.java
@@ -8,4 +8,20 @@ public enum SkyscrapersType {
     SkyscrapersType(int value) {
         this.value = value;
     }
+
+    public static SkyscrapersType convertToSkyType(int num) {
+        if (num == 0) return UNKNOWN;
+        else if (num == 1) return Number;
+        else if (num == 2) return ANY;
+        else if (num == -1) return CLUE_NORTH;
+        else if (num == -2) return CLUE_EAST;
+        else if (num == -3) return CLUE_SOUTH;
+        else if (num == -4) return CLUE_WEST;
+        else {
+            //throw new Exception("conversion from int to SkyscapersType cannot be done; int must be within the range: -4 to 2");
+            // conversion from int to SkyscapersType cannot be done; int must be within the range: -4 to 2"
+            assert (false);
+            return UNKNOWN;
+        }
+    }
 }
