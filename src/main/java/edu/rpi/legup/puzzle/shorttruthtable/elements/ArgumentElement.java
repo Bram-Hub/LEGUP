1866116236d2e510e6d522e942537c6b54bbfad1
charlestian23 <charles.tian23@gmail.com>
2023-03-17 11:39:34 -0400

    More code reformatting

--- src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/ArgumentElement.java
+++ src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/ArgumentElement.java
@@ -2,10 +2,9 @@ package edu.rpi.legup.puzzle.shorttruthtable.elements;
 
 import edu.rpi.legup.model.elements.NonPlaceableElement;
 
-public class ArgumentElement extends NonPlaceableElement
-{
+public class ArgumentElement extends NonPlaceableElement {
     private char letter = 'A';
-    
+
     public ArgumentElement() {
         super("STTT-UNPL-0004", "Argument Element", "Argument of logic statement element", "edu/rpi/legup/images/shorttruthtable/tiles/LetterTile.png");
     }
--- src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/UnknownElement.java
+++ src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/UnknownElement.java
@@ -3,7 +3,7 @@ package edu.rpi.legup.puzzle.shorttruthtable.elements;
 import edu.rpi.legup.model.elements.NonPlaceableElement;
 
 public class UnknownElement extends NonPlaceableElement {
-        public UnknownElement() {
+    public UnknownElement() {
         super("STTT-UNPL-0008", "Unknown Element", "A blank tile", "edu/rpi/legup/images/shorttruthtable/tiles/UnknownTile.png");
     }
 }
