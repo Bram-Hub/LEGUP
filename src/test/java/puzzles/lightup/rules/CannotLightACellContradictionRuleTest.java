16e64dda6a6b9acdc3590af4b9e6dda2da6bf2e7
Jun <song.junyoung64@gmail.com>
2023-03-24 17:21:19 -0400

    Added unit tests tor CannotLightACellContradictionRuleTest and TooFewBulbsContradictionRuleTest.

--- src/test/java/puzzles/lightup/rules/CannotLightACellContradictionRuleTest.java
+++ src/test/java/puzzles/lightup/rules/CannotLightACellContradictionRuleTest.java
@@ -40,4 +40,20 @@ public class CannotLightACellContradictionRuleTest {
 
         Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(1, 1)));
     }
+
+    @Test
+    public void CannotLightACellContradictionRule_CannotFillCorners() throws InvalidFileFormatException  {
+        TestUtilities.importTestBoard("puzzles/lightup/rules/CannotLightACellContradictionRule/CannotFillCorners", lightUp);
+        TreeNode rootNode = lightUp.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(1, 1)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(0, 0)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(0, 2)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(2, 0)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(2, 2)));
+    }
 }
--- src/test/java/puzzles/lightup/rules/TooFewBulbsContradictionRuleTest.java
+++ src/test/java/puzzles/lightup/rules/TooFewBulbsContradictionRuleTest.java
@@ -1,19 +1,100 @@
 package puzzles.lightup.rules;
 
+import edu.rpi.legup.puzzle.lightup.LightUpBoard;
+import edu.rpi.legup.puzzle.lightup.rules.CannotLightACellContradictionRule;
+import edu.rpi.legup.puzzle.lightup.rules.TooFewBulbsContradictionRule;
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import edu.rpi.legup.model.PuzzleImporter;
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import org.junit.Assert;
+import edu.rpi.legup.puzzle.lightup.LightUp;
+import edu.rpi.legup.save.InvalidFileFormatException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
-import edu.rpi.legup.puzzle.lightup.LightUp;
+
 
 public class TooFewBulbsContradictionRuleTest {
+    private static final TooFewBulbsContradictionRule RULE = new TooFewBulbsContradictionRule();
     private static LightUp lightUp;
+    private static PuzzleImporter importer;
 
     @BeforeClass
     public static void setUp() {
+        MockGameBoardFacade.getInstance();
         lightUp = new LightUp();
+        importer = lightUp.getImporter();
     }
 
     @Test
-    public void simpleCaseTest() {
+    public void TooFewBulbsContradictionRule_BlockEnclosed() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/lightup/rules/TooFewBulbsContradictionRule/BlockEnclosed", lightUp);
+        TreeNode rootNode = lightUp.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
 
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 0)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 2)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 0)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 2)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(1, 1)));
+    }
+
+    @Test
+    public void TooFewBulbsContradictionRule_CornerBlockEnclosed() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/lightup/rules/TooFewBulbsContradictionRule/CornerBlockEnclosed", lightUp);
+        TreeNode rootNode = lightUp.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 1)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(1, 0)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(0, 0)));
+    }
+
+    @Test
+    public void TooFewBulbsContradictionRule_ManyBlocksEnclosed() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/lightup/rules/TooFewBulbsContradictionRule/ManyBlocksEnclosed", lightUp);
+        TreeNode rootNode = lightUp.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 0)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(0, 1)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 2)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(1, 0)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(1, 1)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(1, 2)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 0)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 1)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 2)));
+    }
+
+    @Test
+    public void TooFewBulbsContradictionRule_CompleteBoardBlockEnclosed() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/lightup/rules/TooFewBulbsContradictionRule/CompleteBoardBlockEnclosed", lightUp);
+        TreeNode rootNode = lightUp.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 0)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 1)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(0, 2)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(1, 1)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 0)));
+        Assert.assertNull(RULE.checkContradictionAt(board, board.getCell(2, 1)));
+        Assert.assertNotNull(RULE.checkContradictionAt(board, board.getCell(2, 2)));
     }
 }
