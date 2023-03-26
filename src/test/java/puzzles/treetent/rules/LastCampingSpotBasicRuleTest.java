9bd1a8c44bf717f42d07322d3e79492abc46c1cf
jason pu <jason.pu19@outlook.com>
2023-03-26 15:47:48 -0400

    Adds tests for treetent basic rules

---
+++ src/test/java/puzzles/treetent/rules/LastCampingSpotBasicRuleTest.java
@@ -0,0 +1,61 @@
+package puzzles.treetent.rules;
+
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.treetent.TreeTent;
+import edu.rpi.legup.puzzle.treetent.TreeTentBoard;
+import edu.rpi.legup.puzzle.treetent.TreeTentCell;
+import edu.rpi.legup.puzzle.treetent.TreeTentType;
+import edu.rpi.legup.puzzle.treetent.rules.LastCampingSpotBasicRule;
+import edu.rpi.legup.save.InvalidFileFormatException;
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.awt.*;
+
+public class LastCampingSpotBasicRuleTest {
+
+    private static final LastCampingSpotBasicRule RULE = new LastCampingSpotBasicRule();
+    private static TreeTent treetent;
+
+    @BeforeClass
+    public static void setUp() {
+        MockGameBoardFacade.getInstance();
+        treetent = new TreeTent();
+    }
+
+    @Test
+    public void EmptyFieldTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/LastCampingSpotBasicRule/LastCampingSpot", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 0);
+        cell1.setData(TreeTentType.TENT.value);
+
+        board.addModifiedData(cell1);
+
+        Assert.assertNull(RULE.checkRule(transition));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Point point = new Point(k, i);
+                if (point.equals(cell1.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+            }
+        }
+    }
+}
+
+
+
