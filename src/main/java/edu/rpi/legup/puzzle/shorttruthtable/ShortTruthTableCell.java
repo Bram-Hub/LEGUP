d59fe452ac0dc499018c6d198d78f9b5af56aba6
charlestian23 <charles.tian23@gmail.com>
2023-04-04 17:25:32 -0400

    Added puzzle editor cell clicking functionality

--- src/main/java/edu/rpi/legup/puzzle/shorttruthtable/ShortTruthTableCell.java
+++ src/main/java/edu/rpi/legup/puzzle/shorttruthtable/ShortTruthTableCell.java
@@ -1,15 +1,17 @@
 package edu.rpi.legup.puzzle.shorttruthtable;
 
+import edu.rpi.legup.model.elements.Element;
 import edu.rpi.legup.model.gameboard.GridCell;
 
 import edu.rpi.legup.puzzle.shorttruthtable.ShortTruthTableStatement;
 
 import java.awt.Point;
+import java.awt.event.MouseEvent;
 
 public class ShortTruthTableCell extends GridCell<ShortTruthTableCellType> {
 
     //The symbol on the cell
-    private final char symbol;
+    private char symbol;
 
     //This is a reference to the statement that contains this cell
     private ShortTruthTableStatement statement;
@@ -127,5 +129,39 @@ public class ShortTruthTableCell extends GridCell<ShortTruthTableCellType> {
         return copy;
     }
 
+    /**
+     * Sets the type of this ShortTruthTableCell
+     *
+     * @param e element to set the type of this Short Truth Table cell to
+     */
+    @Override
+    public void setType(Element e, MouseEvent m) {
+        if (e.getElementName().equals("Red Element")) {
+            this.data = ShortTruthTableCellType.FALSE;
+        }
+        else if (e.getElementName().equals("Green Element")) {
+            this.data = ShortTruthTableCellType.TRUE;
+        }
+        else if (e.getElementName().equals("Unknown Element")) {
+            this.data = ShortTruthTableCellType.UNKNOWN;
+        }
+        else if (e.getElementName().equals("Argument Element")) {
+            // Prevents non-argument symbols from being changed
+            if (!(this.symbol >= 'A' && this.symbol <= 'Z'))
+                return;
 
+            if (m.getButton() == MouseEvent.BUTTON1) {
+                this.symbol += 1;
+                if (this.symbol > 'Z') {
+                    this.symbol = 'A';
+                }
+            }
+            else if (m.getButton() == MouseEvent.BUTTON3) {
+                this.symbol -= 1;
+                if (this.symbol < 'A') {
+                    this.symbol = 'Z';
+                }
+            }
+        }
+    }
 }
