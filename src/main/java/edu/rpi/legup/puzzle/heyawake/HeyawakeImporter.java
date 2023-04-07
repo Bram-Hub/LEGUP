75ec711bba3fc876b67321cf2ac2980c965c9fa7
Maitri Bijur <122646363+Mbijur@users.noreply.github.com>
2023-04-07 17:09:06 -0400

    Update HeyawakeImporter.java

--- src/main/java/edu/rpi/legup/puzzle/heyawake/HeyawakeImporter.java
+++ src/main/java/edu/rpi/legup/puzzle/heyawake/HeyawakeImporter.java
@@ -30,7 +30,7 @@ public class HeyawakeImporter extends PuzzleImporter {
      * Creates the board for building
      *
      * @param node xml document node
-     * @throws InvalidFileFormatException
+     * @throws InvalidFileFormatException if file is invalid
      */
     @Override
     public void initializeBoard(Node node) throws InvalidFileFormatException {
