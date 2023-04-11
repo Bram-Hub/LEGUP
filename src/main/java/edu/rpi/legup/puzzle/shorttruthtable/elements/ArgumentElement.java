08a8ba7a90df61a04d7e483034dbc9543daa9a78
charlestian23 <charles.tian23@gmail.com>
2023-04-11 14:51:19 -0400

    Only one logic symbol element needed

--- src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/ArgumentElement.java
+++ src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/ArgumentElement.java
@@ -4,6 +4,6 @@ import edu.rpi.legup.model.elements.NonPlaceableElement;
 
 public class ArgumentElement extends NonPlaceableElement {
     public ArgumentElement() {
-        super("STTT-UNPL-0002", "Argument Element", "Argument of logic statement element", "edu/rpi/legup/images/shorttruthtable/tiles/LetterTile.png");
+        super("STTT-UNPL-0001", "Argument Element", "Argument of logic statement element", "edu/rpi/legup/images/shorttruthtable/tiles/LetterTile.png");
     }
 }
--- src/main/java/edu/rpi/legup/puzzle/shorttruthtable/elements/ConditionalBiconditionalElement.java
+++
@@ -1,9 +0,0 @@
-package edu.rpi.legup.puzzle.shorttruthtable.elements;
-
-import edu.rpi.legup.model.elements.NonPlaceableElement;
-
-public class ConditionalBiconditionalElement extends NonPlaceableElement {
-    public ConditionalBiconditionalElement() {
-        super("STTT-UNPL-0003", "Conditional/Biconditional Element", "Conditional/Biconditional logical element", "edu/rpi/legup/images/shorttruthtable/tiles/ConditionalBiconditionalTile.png");
-    }
-}
