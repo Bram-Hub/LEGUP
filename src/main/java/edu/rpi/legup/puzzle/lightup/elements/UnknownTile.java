91e4a483be473481f8d9b08cb77b3c76b74f29c2
rspacerr <definitelynotoberon@gmail.com>
2023-04-25 19:23:16 -0400

    separate files for legup unknown tiles

--- src/main/java/edu/rpi/legup/puzzle/lightup/elements/UnknownTile.java
+++ src/main/java/edu/rpi/legup/puzzle/lightup/elements/UnknownTile.java
@@ -4,7 +4,6 @@ import edu.rpi.legup.model.elements.NonPlaceableElement;
 
 public class UnknownTile extends NonPlaceableElement {
     public UnknownTile() {
-		// This is the same tile as found in Nurikabe, but under LightUp to make it appear in the panel
-        super("NURI-UNPL-0002", "Unknown Tile", "A blank tile", "edu/rpi/legup/images/nurikabe/tiles/UnknownTile.png");
+        super("NURI-UNPL-0002", "Unknown Tile", "A blank tile", "edu/rpi/legup/images/lightup/UnknownTile.png");
     }
 }
