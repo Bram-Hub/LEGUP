5673e0220add7da6ef88fd08e0f778ae6b97a345
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-15 13:13:13 -0700

    Add more tests
    Update TestRunner.java

--- src/test/java/puzzles/battleship/rules/TestRunner.java
+++ src/test/java/puzzles/battleship/rules/TestRunner.java
@@ -5,6 +5,8 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import puzzles.lightup.rules.*;
+import puzzles.nurikabe.rules.*;
+import puzzles.treetent.rules.*;
 
 public class TestRunner {
     public static void main(String[] args) {
@@ -36,6 +38,58 @@ public class TestRunner {
         Result result12 = JUnitCore.runClasses(TooManyBulbsContradictionRuleTest.class);
         printTestResults(result12);
 
+
+
+        //nurikabe tests
+        Result result13 = JUnitCore.runClasses(BlackBetweenRegionsDirectRuleTest.class);
+        printTestResults(result13);
+        Result result14 = JUnitCore.runClasses(BlackBottleNeckDirectRuleTest.class);
+        printTestResults(result14);
+        Result result15 = JUnitCore.runClasses(BlackOrWhiteCaseRuleTest.class);
+        printTestResults(result15);
+        Result result16 = JUnitCore.runClasses(BlackSquareContradictionRuleTest.class);
+        printTestResults(result16);
+        Result result17 = JUnitCore.runClasses(CornerBlackDirectRuleTest.class);
+        printTestResults(result17);
+        Result result18 = JUnitCore.runClasses(FillinBlackDirectRuleTest.class);
+        printTestResults(result18);
+        Result result19 = JUnitCore.runClasses(FillinWhiteDirectRuleTest.class);
+        printTestResults(result19);
+        Result result20 = JUnitCore.runClasses(IsolateBlackContradictionRuleTest.class);
+        printTestResults(result20);
+        Result result21 = JUnitCore.runClasses(MultipleNumbersContradictionRuleTest.class);
+        printTestResults(result21);
+        Result result22 = JUnitCore.runClasses(NoNumbersContradictionRuleTest.class);
+        printTestResults(result22);
+        Result result23 = JUnitCore.runClasses(PreventBlackSquareDirectRuleTest.class);
+        printTestResults(result23);
+        Result result24 = JUnitCore.runClasses(SurroundRegionDirectRuleTest.class);
+        printTestResults(result24);
+        Result result25 = JUnitCore.runClasses(TooFewSpacesContradictionRuleTest.class);
+        printTestResults(result25);
+        Result result26 = JUnitCore.runClasses(TooManySpacesContradictionRuleTest.class);
+        printTestResults(result26);
+        Result result27 = JUnitCore.runClasses(WhiteBottleNeckDirectRuleTest.class);
+        printTestResults(result27);
+
+
+        // Treetent
+        Result result28 = JUnitCore.runClasses(EmptyFieldDirectRuleTest.class);
+        printTestResults(result28);
+        Result result29 = JUnitCore.runClasses(FinishWithGrassDirectRuleTest.class);
+        printTestResults(result29);
+        Result result30 = JUnitCore.runClasses(FinishWithTentsDirectRuleTest.class);
+        printTestResults(result30);
+        Result result31 = JUnitCore.runClasses(LastCampingSpotDirectRuleTest.class);
+        printTestResults(result31);
+        Result result32 = JUnitCore.runClasses(NoTentForTreeContradictionRuleTest.class);
+        printTestResults(result32);
+        Result result33 = JUnitCore.runClasses(NoTreeForTentContradictionRuleTest.class);
+        printTestResults(result33);
+        Result result34 = JUnitCore.runClasses(SurroundTentWithGrassDirectRuleTest.class);
+        printTestResults(result34);
+        Result result35 = JUnitCore.runClasses(TreeForTentDirectRuleTest.class);
+        printTestResults(result35);
     }
 
     private static void printTestResults(Result result) {
