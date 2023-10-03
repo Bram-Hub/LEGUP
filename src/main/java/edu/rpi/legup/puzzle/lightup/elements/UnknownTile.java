0832621a55f4c96f439dafe159a03bccbc4b6e69
charlestian23 <charles.tian23@gmail.com>
2023-10-03 17:04:55 -0400

    Added unknown tile stuff

---
+++ src/main/java/edu/rpi/legup/puzzle/lightup/elements/UnknownTile.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.lightup.elements;
+
+import edu.rpi.legup.model.elements.NonPlaceableElement;
+
+public class UnknownTile extends NonPlaceableElement {
+    public UnknownTile() {
+        super("NURI-UNPL-0003", "Unknown Tile", "A blank tile", "edu/rpi/legup/images/lightup/UnknownTile.png");
+    }
+}
