23d6558aa567c50906318f2fc6518a94dd653593
charlestian23 <charles.tian23@gmail.com>
2023-10-03 17:07:12 -0400

    ID error

--- src/main/java/edu/rpi/legup/puzzle/lightup/LightUpCell.java
+++ src/main/java/edu/rpi/legup/puzzle/lightup/LightUpCell.java
@@ -1,8 +1,10 @@
 package edu.rpi.legup.puzzle.lightup;
 
+import edu.rpi.legup.model.elements.Element;
 import edu.rpi.legup.model.gameboard.GridCell;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class LightUpCell extends GridCell<Integer> {
     private boolean isLite;
@@ -10,6 +12,21 @@ public class LightUpCell extends GridCell<Integer> {
     public LightUpCell(int valueInt, Point location) {
         super(valueInt, location);
         this.isLite = false;
+    }
+
+    @Override
+    public void setType(Element e, MouseEvent m) {
+        switch (e.getElementID()) {
+            case "LTUP-PLAC-0001":
+                this.data = -4;
+                break;
+            case "LTUP-UNPL-0002":
+                this.data = -1;
+                break;
+            case "LTUP-UNPL-0003":
+                this.data = -2;
+                break;
+        }
     }
 
     public LightUpCellType getType() {
