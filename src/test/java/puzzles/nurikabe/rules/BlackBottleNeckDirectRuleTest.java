18d1ab64cd223aacab4ec583c9d447f49656182c
Relurk1 <keshavg80@gmail.com>
2023-10-05 11:03:46 -0400

    Added test cases for Black Bottleneck rule
    Added two new test cases for the Black Bottleneck direct rule.
    
    The first test case checks whether bottlenecks can be successfully detected in a corner
    
    The second test case intuitively looks like a bottleneck, but is not actually a bottleneck. This is to avoid false positives.

--- src/test/java/puzzles/nurikabe/rules/BlackBottleNeckDirectRuleTest.java
+++ src/test/java/puzzles/nurikabe/rules/BlackBottleNeckDirectRuleTest.java
@@ -54,4 +54,55 @@ public class BlackBottleNeckDirectRuleTest {
             }
         }
     }
+    @Test
+    public void BlackBottleNeckDirectRule_CornerBottleneck() throws InvalidFileFormatException{
+        TestUtilities.importTestBoard("puzzles/nurikabe/rules/BlackBottleNeckDirectRule/CornerBottleNeck", nurikabe);
+        TreeNode rootNode = nurikabe.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        NurikabeBoard board = (NurikabeBoard)transition.getBoard();
+        NurikabeCell cell = board.getCell(0,1);
+        cell.setData(NurikabeType.BLACK.toValue());
+
+        board.addModifiedData(cell);
+
+        Assert.assertNull(RULE.checkRule(transition));
+
+        for(int i = 0; i < board.getHeight(); i++){
+            for(int k = 0; k < board.getWidth(); k++){
+                Point point  = new Point(k, i);
+                if(point.equals(cell.getLocation())){
+                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void BlackBottleNeckDirectRule_FalseBottleneck() throws InvalidFileFormatException{
+        TestUtilities.importTestBoard("puzzles/nurikabe/rules/BlackBottleNeckDirectRule/FalseBottleNeck", nurikabe);
+        TreeNode rootNode = nurikabe.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        NurikabeBoard board = (NurikabeBoard)transition.getBoard();
+        NurikabeCell cell = board.getCell(0,1);
+        cell.setData(NurikabeType.BLACK.toValue());
+
+        board.addModifiedData(cell);
+
+        Assert.assertNotNull(RULE.checkRule(transition));
+
+        for(int i = 0; i < board.getHeight(); i++){
+            for(int k = 0; k < board.getWidth(); k++){
+                Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+            }
+        }
+    }
 }
+
+
