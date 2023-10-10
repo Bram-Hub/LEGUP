f2b1cfaa461895f7c59bee6e5224921d42b05246
charlestian23 <charles.tian23@gmail.com>
2023-10-10 14:44:55 -0400

    Added another dummy test

---
+++ src/test/java/puzzles/shorttruthtable/rules/DummyTest.java
@@ -0,0 +1,13 @@
+package puzzles.shorttruthtable.rules;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class DummyTest {
+
+    @Test
+    public void testFailure() {
+        fail("This test intentionally fails. Your JUnit configuration is working if you see this message.");
+    }
+}
