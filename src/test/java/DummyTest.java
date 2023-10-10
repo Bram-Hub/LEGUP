19f0c26a52e1a674d3d9235345c932e2606ae16e
charlestian23 <charles.tian23@gmail.com>
2023-10-10 14:42:49 -0400

    Create DummyTest.java
    For debugging purposes

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class DummyTest {
+
+    @Test
+    public void testFailure() {
+        fail("This test intentionally fails. Your JUnit configuration is working if you see this message.");
+    }
+}
