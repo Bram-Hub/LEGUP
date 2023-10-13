c98c0f69d32063e1e2041b19c7023c6fb0837ac7
charlestian23 <charles.tian23@gmail.com>
2023-10-13 16:57:51 -0400

    Working initial test

--- src/test/java/puzzles/shorttruthtable/rules/AtomicDirectRuleTest.java
+++ src/test/java/puzzles/shorttruthtable/rules/AtomicDirectRuleTest.java
@@ -1,11 +1,25 @@
 package puzzles.shorttruthtable.rules;
 
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
 import edu.rpi.legup.puzzle.shorttruthtable.ShortTruthTable;
+import edu.rpi.legup.puzzle.shorttruthtable.ShortTruthTableBoard;
+import edu.rpi.legup.puzzle.shorttruthtable.ShortTruthTableCell;
+import edu.rpi.legup.puzzle.shorttruthtable.ShortTruthTableCellType;
 import edu.rpi.legup.puzzle.shorttruthtable.rules.basic.DirectRuleAtomic;
+import edu.rpi.legup.puzzle.treetent.TreeTentBoard;
+import edu.rpi.legup.puzzle.treetent.TreeTentCell;
+import edu.rpi.legup.puzzle.treetent.TreeTentType;
+import edu.rpi.legup.save.InvalidFileFormatException;
 import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
-class AtomicDirectRuleTest {
+import java.awt.*;
+
+public class AtomicDirectRuleTest {
     private static final DirectRuleAtomic RULE = new DirectRuleAtomic();
     private static ShortTruthTable stt;
 
@@ -14,5 +28,21 @@ class AtomicDirectRuleTest {
     {
         MockGameBoardFacade.getInstance();
         stt = new ShortTruthTable();
+    }
+
+    @Test
+    public void MatchingFalseTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/shorttruthtable/rules/AtomicDirectRule/FalseA", stt);
+        TreeNode rootNode = stt.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        ShortTruthTableBoard board = (ShortTruthTableBoard) transition.getBoard();
+
+        ShortTruthTableCell cell = board.getCell(0, 2);
+        cell.setData(ShortTruthTableCellType.FALSE);
+        board.addModifiedData(cell);
+
+        Assert.assertNull(RULE.checkRule(transition));
     }
 }
