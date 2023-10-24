16a8f0f1ba347037e11a2a7981adbc24a310bc9e
Rorymar <36866664+Rorymar@users.noreply.github.com>
2023-10-24 16:51:13 -0400

    TooFewTents Tests
    Added more expansive tests for the TooFewTents rule

--- src/test/java/puzzles/treetent/rules/TooFewTentsContradictionRuleTest.java
+++ src/test/java/puzzles/treetent/rules/TooFewTentsContradictionRuleTest.java
@@ -10,12 +10,8 @@ import org.junit.Test;
 
 import edu.rpi.legup.puzzle.treetent.TreeTent;
 import edu.rpi.legup.puzzle.treetent.TreeTentBoard;
-import edu.rpi.legup.puzzle.treetent.TreeTentCell;
-import edu.rpi.legup.puzzle.treetent.TreeTentType;
 import edu.rpi.legup.puzzle.treetent.rules.TooFewTentsContradictionRule;
 import edu.rpi.legup.save.InvalidFileFormatException;
-
-import java.awt.*;
 
 public class TooFewTentsContradictionRuleTest {
 
@@ -28,9 +24,46 @@ public class TooFewTentsContradictionRuleTest {
         treetent = new TreeTent();
     }
 
+    /**
+     * @throws InvalidFileFormatException
+     * Using a 1x1 Puzzle Grid, which is just grass, checks if the fact it expects a tent on the y-axis is caught.
+     */
     @Test
-    public void TooFewTentsContradictionRule_() throws InvalidFileFormatException {
-        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTents", treetent);
+    public void TooFewTentsContradictionRule_JustY() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTentsJustY", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+        Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(0, 0)));
+    }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Using a 1x1 Puzzle Grid, which is just a tent, checks if the fact it expects 2 tents on the y-axis is caught.
+     * (This is an impossible situation given the constraints, but for the purposes of the test it is fine)
+     */
+    @Test
+    public void TooFewTentsContradictionRule_WithTent() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTentsWithTent", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+        Assert.assertNull(RULE.checkContradiction(board));
+        Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(0, 0)));
+    }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Using a 1x1 Puzzle Grid, which is just grass, checks if the fact it expects a tent on both x and y is caught.
+     */
+    @Test
+    public void TooFewTentsContradictionRule_DoubleBad() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTentsDoubleBad", treetent);
         TreeNode rootNode = treetent.getTree().getRootNode();
         TreeTransition transition = rootNode.getChildren().get(0);
         transition.setRule(RULE);
