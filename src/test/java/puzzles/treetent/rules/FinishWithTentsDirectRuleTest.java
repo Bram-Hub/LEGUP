8b34ffc50196ae44487f08a0c4bc5a945b399ec2
Kevin-771 <70790256+Kevin-771@users.noreply.github.com>
2023-10-27 16:49:29 -0400

    added some tests

--- src/test/java/puzzles/treetent/rules/FinishWithTentsDirectRuleTest.java
+++ src/test/java/puzzles/treetent/rules/FinishWithTentsDirectRuleTest.java
@@ -15,6 +15,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.*;
 
 public class FinishWithTentsDirectRuleTest {
 
@@ -28,8 +29,8 @@ public class FinishWithTentsDirectRuleTest {
     }
 
     @Test
-    public void EmptyFieldTest() throws InvalidFileFormatException {
-        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/FinishWithTents", treetent);
+    public void FinishWithHorizontalTentsTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/FinishWithHorizontalTents", treetent);
         TreeNode rootNode = treetent.getTree().getRootNode();
         TreeTransition transition = rootNode.getChildren().get(0);
         transition.setRule(RULE);
@@ -48,13 +49,189 @@ public class FinishWithTentsDirectRuleTest {
 
         for (int i = 0; i < board.getHeight(); i++) {
             for (int k = 0; k < board.getWidth(); k++) {
-                Point point = new Point(k, i);
-                if (point.equals(cell1.getLocation()) || point.equals(cell2.getLocation())) {
-                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                TreeTentCell c = board.getCell(k, i);
+                if ((c.getLocation()).equals(cell1.getLocation()) || (c.getLocation()).equals(cell2.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, c));
                 }
                 else {
-                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, c));
                 }
+            }
+        }
+    }
+
+    @Test
+    public void FinishWithVerticalTentsTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/FinishWithVerticalTents", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(0, 1);
+        cell1.setData(TreeTentType.TENT);
+        TreeTentCell cell2 = board.getCell(0, 2);
+        cell2.setData(TreeTentType.TENT);
+
+        board.addModifiedData(cell1);
+        board.addModifiedData(cell2);
+
+        Assert.assertNull(RULE.checkRule(transition));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                TreeTentCell c = board.getCell(k, i);
+                if ((c.getLocation()).equals(cell1.getLocation()) || (c.getLocation()).equals(cell2.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, c));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, c));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void FinishWithTentsTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/FinishWithTents", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 0);
+        TreeTentCell cell2 = board.getCell(1, 2);
+        TreeTentCell cell3 = board.getCell(0, 1);
+        TreeTentCell cell4 = board.getCell(2, 1);
+
+        cell1.setData(TreeTentType.TENT);
+        cell2.setData(TreeTentType.TENT);
+        cell3.setData(TreeTentType.TENT);
+        cell4.setData(TreeTentType.TENT);
+
+        board.addModifiedData(cell1);
+        board.addModifiedData(cell2);
+        board.addModifiedData(cell3);
+        board.addModifiedData(cell4);
+
+        Assert.assertNull(RULE.checkRule(transition));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                TreeTentCell c = board.getCell(k, i);
+                if ((c.getLocation()).equals(cell1.getLocation()) ||
+                    (c.getLocation()).equals(cell2.getLocation()) ||
+                    (c.getLocation()).equals(cell3.getLocation()) ||
+                    (c.getLocation()).equals(cell4.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, c));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, c));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void AdditionalTentsTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/AdditionalTents", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 0);
+        TreeTentCell cell2 = board.getCell(1, 2);
+        TreeTentCell cell3 = board.getCell(0, 1);
+        TreeTentCell cell4 = board.getCell(2, 1);
+
+        cell1.setData(TreeTentType.TENT);
+        cell2.setData(TreeTentType.TENT);
+        cell3.setData(TreeTentType.TENT);
+        cell4.setData(TreeTentType.TENT);
+
+        board.addModifiedData(cell1);
+        board.addModifiedData(cell2);
+        board.addModifiedData(cell3);
+        board.addModifiedData(cell4);
+
+        Assert.assertNull(RULE.checkRule(transition));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                TreeTentCell c = board.getCell(k, i);
+                if ((c.getLocation()).equals(cell1.getLocation()) ||
+                    (c.getLocation()).equals(cell2.getLocation()) ||
+                    (c.getLocation()).equals(cell3.getLocation()) ||
+                    (c.getLocation()).equals(cell4.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, c));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, c));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void FinishWithTentsFailTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/FinishWithTentsFail", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        ArrayList<TreeTentCell> cells = new ArrayList<TreeTentCell>();
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                TreeTentCell c = board.getCell(k, i);
+                c.setData(TreeTentType.TENT);
+                board.addModifiedData(c);
+                cells.add(c);
+            }
+        }
+
+        Assert.assertNotNull(RULE.checkRule(transition));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+            }
+        }
+    }
+
+    @Test
+    public void TooManyTentsTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/FinishWithTentsDirectRule/TooManyTents", treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        ArrayList<TreeTentCell> cells = new ArrayList<TreeTentCell>();
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                if((k == 1)&&(i == 1))
+                    continue;
+                TreeTentCell c = board.getCell(k, i);
+                c.setData(TreeTentType.TENT);
+                board.addModifiedData(c);
+                cells.add(c);
+            }
+        }
+
+        Assert.assertNotNull(RULE.checkRule(transition));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
             }
         }
     }
