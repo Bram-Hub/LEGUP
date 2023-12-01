2e100bd83d10443671eefb39e61d81c6792ae985
Steven Cadogan <cadogs@rpi.edu>
2023-12-01 16:48:07 -0500

    FinishRoomCaseRule initial completed attempt
    First clean attempt at rule.

--- src/main/java/edu/rpi/legup/puzzle/nurikabe/rules/FinishRoomCaseRule.java
+++ src/main/java/edu/rpi/legup/puzzle/nurikabe/rules/FinishRoomCaseRule.java
@@ -51,11 +51,6 @@ public class FinishRoomCaseRule extends CaseRule {
             if (((NurikabeCell) t1.getBoard().getModifiedData().iterator().next()).getType() != NurikabeType.WHITE) {
                 return super.getInvalidUseOfRuleMessage() + ": This case rule must place an empty white cell for each case.";
             }
-//            for (TreeTransition t2 : childTransitions) {
-//                if (t1 != t2 && t1.getLocation().equals(t2.getLocation())) {
-//
-//                }
-//            }
             for (Point loc : locations) {
                 for (Point loc2 : locations) {
                     if ((loc != loc2) && (loc.x == loc2.x) && (loc.y == loc2.y)) {
@@ -66,136 +61,6 @@ public class FinishRoomCaseRule extends CaseRule {
         }
 
         return null;
-//
-////        TreeTransition case1 = childTransitions.get(0);
-////        TreeTransition case2 = childTransitions.get(1);
-////        if (case1.getBoard().getModifiedData().size() != 1 ||
-////                case2.getBoard().getModifiedData().size() != 1) {
-////            return super.getInvalidUseOfRuleMessage() + ": This case rule must have 1 modified cell for each case.";
-////        }
-//
-////        NurikabeCell mod1 = (NurikabeCell) case1.getBoard().getModifiedData().iterator().next();
-////        NurikabeCell mod2 = (NurikabeCell) case2.getBoard().getModifiedData().iterator().next();
-////        if (!(mod1.getType() == NurikabeType.WHITE && mod2.getType() == NurikabeType.WHITE)) {
-////            return super.getInvalidUseOfRuleMessage() + ": This case rule must place 2 empty white cells.";
-////        }
-//        if (mod1.getLocation().equals(mod2.getLocation())) {
-//            return super.getInvalidUseOfRuleMessage() + ": This case rule must modify a different cell for each case.";
-//        }
-//
-//        Point loc1 = mod1.getLocation(); //position of placed cell
-//        Point loc2 = mod2.getLocation(); //position of other placed cell
-//        Set<NurikabeCell> adj = new HashSet<>(); //set to hold adjacent cells
-//        Set<NurikabeCell> adj2 = new HashSet<>(); //set to hold adjacent cells
-//        Point left = new Point(-1,0);
-//        Point right = new Point(1, 0);
-//        Point bot = new Point(0, -1);
-//        Point top = new Point(0, 1);
-//        Set<Point> directions = new HashSet<>();
-//        directions.add(left);
-//        directions.add(right);
-//        directions.add(top);
-//        directions.add(bot);
-//        for(Point direction : directions) {
-//            NurikabeCell curr = destBoardState.getCell(loc1.x + direction.x, loc1.y + direction.y);
-//            if(curr != null) {
-//                if(curr.getType() == NurikabeType.WHITE || curr.getType() == NurikabeType.NUMBER) {
-//                    adj.add(curr); //adds cells to adj only if they are white or number blocks
-//                }
-//            }
-//            NurikabeCell curr2 = destBoardState.getCell(loc2.x + direction.x, loc2.y + direction.y);
-//            if(curr2 != null) {
-//                if(curr2.getType() == NurikabeType.WHITE || curr2.getType() == NurikabeType.NUMBER) {
-//                    adj2.add(curr2);
-//                }
-//            }
-//        }
-//        boolean match = false;
-//        DisjointSets<NurikabeCell> regions = NurikabeUtilities.getNurikabeRegions(destBoardState);
-//        Set<NurikabeCell> realadj = new HashSet<>(); //set to hold adjacent cells
-//        Set<NurikabeCell> realadj2 = new HashSet<>(); //set to hold adjacent cells
-//        boolean breakit = false;
-//        for (NurikabeCell c : adj) { //loops through adjacent cells
-//            breakit = false;
-////            Set<NurikabeCell> disRow = regions.getSet(c); //set of white spaces
-////            for (NurikabeCell d : disRow) { //loops through white spaces
-//                for (NurikabeCell c2 : adj2) {
-//                    Set<NurikabeCell> disRow2 = regions.getSet(c2);
-//                    Set<NurikabeCell> disRow = regions.getSet(c); //set of white spaces
-//                    breakit = false;
-//                    for (NurikabeCell d : disRow) { //loops through white spaces
-//                    for (NurikabeCell d2 : disRow2) {
-//                        if(d.getLocation() == d2.getLocation()) {
-//                            match = true;
-//                            realadj.add(c);
-//                            realadj2.add(d);
-//                            breakit = true;
-//                            break;
-//                        }
-//                    }
-//                    if(breakit) {
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//        //the above should find and add to realadj and realadj2 all of the adjacent cells to the original
-//        //that match with the same white space region that has the room you're trying to finish
-//
-//        if(!match) {
-//            return super.getInvalidUseOfRuleMessage() + ": This case rule must modify cells adjacent to the same white region.";
-//        }
-//
-//        match = false;
-//
-//        //issue is ensuring that the 2 modified locations are related to the same white region. can't just make sure
-//        //white number size is same because there can be multiple that are the same
-//        //maybe point of white number being the same??
-//        int intArray[] = new int[realadj.size()];
-//        for(int k = 0; k < realadj.size(); k++) {
-//            intArray[k] = -1;
-//        }
-//        int count = 0;
-//        for (NurikabeCell c : realadj) { //loops through adjacent cells //changed to realadj
-//            Set<NurikabeCell> disRow = regions.getSet(c); //set of white spaces
-//            //if the location of any of the blocks is the same. User should realize that solution can't be made
-//            //if they correctly fill one room but it adds too many squares to another already completed room
-//            for (NurikabeCell d : disRow) { //loops through white spaces
-//                if (d.getType() == NurikabeType.NUMBER) { //if the white space is a number
-//                    if(regions.getSet(d).size()-1 == d.getData()) { //if that cells white area is 1 less than
-//                        //return null; //the size of the number of one of the number cells within that set
-//                        match = true;
-//                        intArray[count] = count;
-//                    }
-//                }
-//            }
-//            count++;
-//        }
-//        if(!match) {
-//            return super.getInvalidUseOfRuleMessage() + ": This case rule must modify cells adjacent to the same white region that " +
-//                    "needs one more white cell to complete the room.";
-//        }
-//
-//        count = 0;
-//        for (NurikabeCell c : realadj2) { //loops through adjacent cells //changed to realadj2
-//            Set<NurikabeCell> disRow = regions.getSet(c); //set of white spaces
-//            //if the location of any of the blocks is the same. User should realize that solution can't be made
-//            //if they correctly fill one room but it adds too many squares to another already completed room
-//            for (NurikabeCell d : disRow) { //loops through white spaces
-//                if (d.getType() == NurikabeType.NUMBER) { //if the white space is a number
-//                    if(regions.getSet(d).size()-1 == d.getData() && intArray[count] == count) { //if that cells white area is 1 less than
-//                        //return null; //the size of the number of one of the number cells within that set
-//                        return null;
-//                    }
-//                }
-//            }
-//            count++;
-//        }
-//        //if the cell is white and size of its column is 1 less than the room size
-//        //may not work for example with room of size 4 that is finished. But also a near adjacent room of size
-//        //3 that is also finished. Would validate the 4 - 1 = 3 even tho the 3 room is finished.
-//        return super.getInvalidUseOfRuleMessage() + ": This case rule must modify cells adjacent to the same white region that " +
-//                "needs one more white cell to complete the room.";
     }
 
     @Override
@@ -204,40 +69,6 @@ public class FinishRoomCaseRule extends CaseRule {
         CaseBoard caseBoard = new CaseBoard(nurikabeBoard, this);
         DisjointSets<NurikabeCell> regions = NurikabeUtilities.getNurikabeRegions(nurikabeBoard);
         nurikabeBoard.setModifiable(false);
-//        Point left = new Point(-1,0);
-//        Point right = new Point(1, 0);
-//        Point bot = new Point(0, -1);
-//        Point top = new Point(0, 1);
-//        Set<Point> directions = new HashSet<>();
-//        directions.add(left);
-//        directions.add(right);
-//        directions.add(top);
-//        directions.add(bot);
-//        for (PuzzleElement element : nurikabeBoard.getPuzzleElements()) {
-//            for(Point direction : directions) {
-//                NurikabeCell curr = nurikabeBoard.getCell(((NurikabeCell) element).getLocation().x + direction.x,
-//                        ((NurikabeCell) element).getLocation().y + direction.y);
-//                if (curr.getType() == NurikabeType.WHITE || curr.getType() == NurikabeType.UNKNOWN) {
-//                    ArrayList<NurikabeCell> numberedCells = new ArrayList<>();
-//                    Set<NurikabeCell> disRow = regions.getSet(curr); //set of white spaces
-//                    for (NurikabeCell d : disRow) { //loops through white spaces
-//                        if (d.getType() == NurikabeType.NUMBER) { //if the white space is a number
-//                            numberedCells.add(d); //add that number to numberedCells
-//                        }
-//                    }
-//                    for (NurikabeCell number : numberedCells) { //loops through numberedCells
-//                        if (regions.getSet(number).size()-1 == number.getData()) { //if that cells white area is the exact
-//                            caseBoard.addPickableElement(element);
-//                            break;
-//                        }
-//                    }
-//
-////                    caseBoard.addPickableElement(element);
-////                    break;
-//                }
-//            }
-//        }
-
 
 
         for (PuzzleElement element : nurikabeBoard.getPuzzleElements()) { //loops all puzzle elements
@@ -316,7 +147,6 @@ public class FinishRoomCaseRule extends CaseRule {
                 }
             }
         }
-
         return cases;
     }
 
