51e20dbdae93fc5312d3151b235348d8cef32519
Rorymar <36866664+Rorymar@users.noreply.github.com>
2023-12-01 17:24:30 -0500

    TooFewTents Overhaul

--- src/test/java/puzzles/treetent/rules/TooFewTentsContradictionRuleTest.java
+++ src/test/java/puzzles/treetent/rules/TooFewTentsContradictionRuleTest.java
@@ -1,5 +1,6 @@
 package puzzles.treetent.rules;
 
+import edu.rpi.legup.puzzle.treetent.TreeTentCell;
 import legup.MockGameBoardFacade;
 import legup.TestUtilities;
 import edu.rpi.legup.model.tree.TreeNode;
@@ -12,6 +13,8 @@ import edu.rpi.legup.puzzle.treetent.TreeTent;
 import edu.rpi.legup.puzzle.treetent.TreeTentBoard;
 import edu.rpi.legup.puzzle.treetent.rules.TooFewTentsContradictionRule;
 import edu.rpi.legup.save.InvalidFileFormatException;
+
+import java.awt.*;
 
 public class TooFewTentsContradictionRuleTest {
 
@@ -72,5 +75,177 @@ public class TooFewTentsContradictionRuleTest {
         Assert.assertNull(RULE.checkContradiction(board));
         Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(0, 0)));
     }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Looks at a 2x2 Board in the format:
+     *  [] Tr
+     *  [] Gr
+     *  Column 2 is checked to have 1 Tent (which is not present, thus producing a contradiction)
+     */
+    @Test
+    public void TooFewTentsContradictionRule_2x2ColumnOnly() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTents2x2Column",treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        Assert.assertNull(RULE.checkContradiction(board));
+
+        TreeTentCell cell1 = board.getCell(1,0);
+        TreeTentCell cell2 = board.getCell(1,1);
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Point point = new Point(k, i);
+                if (point.equals(cell1.getLocation()) || point.equals(cell2.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Looks at a 2x2 Board in the format:
+     *  Tr Gr
+     *  [] []
+     *  Row 1 is checked to have 1 Tent (which is not present, thus producing a contradiction)
+     */
+    @Test
+    public void TooFewTentsContradictionRule_2x2RowOnly() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTents2x2Row",treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        Assert.assertNull(RULE.checkContradiction(board));
+
+        TreeTentCell cell1 = board.getCell(0,0);
+        TreeTentCell cell2 = board.getCell(1,0);
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Point point = new Point(k, i);
+                if (point.equals(cell1.getLocation()) || point.equals(cell2.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Looks at a 3x3 Board in the format:
+     *  [] Tr []
+     *  [] Gr []
+     *  [] Gr []
+     *  Column 2 is checked to have 1 Tent (which is not present, thus producing a contradiction)
+     */
+    @Test
+    public void TooFewTentsContradictionRule_3x3OneColumn() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTents3x3Column",treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        Assert.assertNull(RULE.checkContradiction(board));
+
+        TreeTentCell cell1 = board.getCell(1,0);
+        TreeTentCell cell2 = board.getCell(1,1);
+        TreeTentCell cell3 = board.getCell(1,2);
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Point point = new Point(k, i);
+                if (point.equals(cell1.getLocation()) || point.equals(cell2.getLocation()) || point.equals(cell3.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Looks at a 3x3 Board in the format:
+     *  Gr Tr Gr
+     *  Gr [] Gr
+     *  Gr Tr Gr
+     *  Column 1 and 3 are checked to have 1 Tent (which is not present, thus producing a contradiction)
+     */
+    @Test
+    public void TooFewTentsContradictionRule_3x3TwoColumn() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTents3x3DoubleColumn",treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        Assert.assertNull(RULE.checkContradiction(board));
+
+        TreeTentCell cell1 = board.getCell(0,0);
+        TreeTentCell cell2 = board.getCell(0,1);
+        TreeTentCell cell3 = board.getCell(0,2);
+        TreeTentCell cell4 = board.getCell(2,0);
+        TreeTentCell cell5 = board.getCell(2,1);
+        TreeTentCell cell6 = board.getCell(2,2);
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Point point = new Point(k, i);
+                if (point.equals(cell1.getLocation()) || point.equals(cell2.getLocation()) || point.equals(cell3.getLocation()) ||
+                    point.equals(cell4.getLocation()) || point.equals(cell5.getLocation()) || point.equals(cell6.getLocation())) {
+                    Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+                else {
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+                }
+            }
+        }
+    }
+
+    /**
+     * @throws InvalidFileFormatException
+     * Looks at a 2x2 Board in the format:
+     *  Tn []
+     *  Tr []
+     * This should fail the contradiction as it is a legal board.
+     */
+    @Test
+    public void TooFewTentsContradictionRule_NoContradiction() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TooFewTentsContradictionRule/TooFewTentsNoContradiction",treetent);
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        Assert.assertNotNull(RULE.checkContradiction(board));
+
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int k = 0; k < board.getWidth(); k++) {
+                Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(k, i)));
+            }
+        }
+    }
+
+
+
 }
 
