14b0750264664ca0c97b1b4dd32e29e91152dd85
Kevin-771 <70790256+Kevin-771@users.noreply.github.com>
2023-12-14 19:33:56 -0500

    added TentOrGrassCaseRuleTests (#688)
    * added a test
    
    * added more to test
    
    * test
    
    * should work now
    
    * should work now
    
    ---------
    
    Co-authored-by: Charles Tian <46334090+charlestian23@users.noreply.github.com>

--- src/test/java/puzzles/treetent/rules/TentOrGrassCaseRuleTest.java
+++ src/test/java/puzzles/treetent/rules/TentOrGrassCaseRuleTest.java
@@ -1,14 +1,12 @@
 package puzzles.treetent.rules;
 
 import edu.rpi.legup.model.gameboard.Board;
-import edu.rpi.legup.model.tree.Tree;
 import edu.rpi.legup.model.tree.TreeNode;
 import edu.rpi.legup.model.tree.TreeTransition;
 import edu.rpi.legup.puzzle.treetent.TreeTent;
 import edu.rpi.legup.puzzle.treetent.TreeTentBoard;
 import edu.rpi.legup.puzzle.treetent.TreeTentCell;
 import edu.rpi.legup.puzzle.treetent.TreeTentType;
-import edu.rpi.legup.puzzle.treetent.rules.FillinRowCaseRule;
 import edu.rpi.legup.puzzle.treetent.rules.TentOrGrassCaseRule;
 import edu.rpi.legup.save.InvalidFileFormatException;
 import legup.MockGameBoardFacade;
@@ -23,32 +21,63 @@ import java.util.ArrayList;
 public class TentOrGrassCaseRuleTest {
     private static final TentOrGrassCaseRule RULE = new TentOrGrassCaseRule();
     private static TreeTent treetent;
-
+ 
     @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
         MockGameBoardFacade.getInstance();
         treetent = new TreeTent();
     }
 
     /**
+     * empty 3x3 TreeTent puzzle
+     * Tests TentOrGrassCaseRule on UNKOWN tile
+     * at (0,0)
+     * 
+     * checks 2 cases are created
+     * checks first case is TENT tile
+     * checks second case is GRASS tile
+     * checks other cells have not been modified
+     * 
      * @throws InvalidFileFormatException
-     * A temporary test
      */
     @Test
-    public void TentOrGrassCaseRule_Test() throws InvalidFileFormatException {
-        TestUtilities.importTestBoard("puzzles/treetent/rules/TentOrGrassCaseRule/TentOrGrassTest", treetent);
+    public void TentOrTreeTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/treetent/rules/TentOrGrassCaseRule/TestPuzzle", treetent);
         TreeNode rootNode = treetent.getTree().getRootNode();
         TreeTransition transition = rootNode.getChildren().get(0);
         transition.setRule(RULE);
 
         TreeTentBoard board = (TreeTentBoard) transition.getBoard();
-        TreeTentCell testing_cell = board.getCell(1, 0);
+        TreeTentCell testing_cell = board.getCell(0, 0);
         ArrayList<Board> cases = RULE.getCases(board, testing_cell);
+
         // assert correct number of cases created
         Assert.assertEquals(2, cases.size());
-        //Store the 0,1 cells from each case
-        //Assert that the Array of their states match to a an array of the expected.
 
+        // TENT case
+        TreeTentBoard tentCase = (TreeTentBoard) cases.get(0);
+        Assert.assertEquals(tentCase.getCell(0, 0).getType(), TreeTentType.TENT);
+
+        // GRASS case
+        TreeTentBoard grassCase = (TreeTentBoard) cases.get(1);
+        Assert.assertEquals(grassCase.getCell(0, 0).getType(), TreeTentType.GRASS);
+
+        // checks other cells have not been modified
+        TreeTentCell original_cell;
+        TreeTentCell case_cell;
+
+        for (int w =0; w < board.getWidth(); w++) {
+            for (int h = 0; h < board.getHeight(); h++) {
+                if (w == 0 && h ==0) {
+                    continue;
+                }
+                original_cell = board.getCell(w, h);
+                case_cell = tentCase.getCell(w, h);
+                Assert.assertEquals(original_cell.getType(), case_cell.getType());
+
+                case_cell = grassCase.getCell(w, h);
+                Assert.assertEquals(original_cell.getType(), case_cell.getType());
+            }
+        }
     }
-
 }
