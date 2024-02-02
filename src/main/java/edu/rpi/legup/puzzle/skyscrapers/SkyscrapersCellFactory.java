3bfa064f79d42b8bc6c7b430a4c2a882e538284c
Jaden Tian <jaden.tian@gmail.com>
2024-02-01 20:29:07 -0500

    Remove Extraneous Code
    Removed functionality required for TreeTent but unnecessary for Skyscrapers

--- src/main/java/edu/rpi/legup/puzzle/skyscrapers/SkyscrapersCellFactory.java
+++ src/main/java/edu/rpi/legup/puzzle/skyscrapers/SkyscrapersCellFactory.java
@@ -19,54 +19,37 @@ public class SkyscrapersCellFactory extends ElementFactory {
      * @return newly created cell from the xml document Node
      * @throws InvalidFileFormatException if input is invalid
      */
-    // Never gets called?
     @Override
     public PuzzleElement importCell(Node node, Board board) throws InvalidFileFormatException {
         try {
+            if (!node.getNodeName().equalsIgnoreCase("cell")) {
+                throw new InvalidFileFormatException("Skyscrapers Factory: unknown puzzleElement puzzleElement");
+            }
+
             SkyscrapersBoard skyscrapersBoard = (SkyscrapersBoard) board;
-            int width = skyscrapersBoard.getWidth();
-            int height = skyscrapersBoard.getHeight();
+            int size = skyscrapersBoard.getSize();
             NamedNodeMap attributeList = node.getAttributes();
-            if (node.getNodeName().equalsIgnoreCase("cell")) {
 
-                int value = Integer.valueOf(attributeList.getNamedItem("value").getNodeValue());
-                int x = Integer.valueOf(attributeList.getNamedItem("x").getNodeValue());
-                int y = Integer.valueOf(attributeList.getNamedItem("y").getNodeValue());
-                if (x >= width || y >= height) {
-                    throw new InvalidFileFormatException("Skyscrapers Factory: cell location out of bounds");
-                }
-                if (value < 0 || value > width) {
-                    throw new InvalidFileFormatException("Skyscrapers Factory: cell unknown value");
-                }
-
-                SkyscrapersCell cell = new SkyscrapersCell(value, new Point(x, y), width);
-                cell.setIndex(y * height + x);
-                return cell;
+            int value = Integer.valueOf(attributeList.getNamedItem("value").getNodeValue());
+            int x = Integer.valueOf(attributeList.getNamedItem("x").getNodeValue());
+            int y = Integer.valueOf(attributeList.getNamedItem("y").getNodeValue());
+            if (x >= size || y >= size) {
+                throw new InvalidFileFormatException("Skyscrapers Factory: cell location out of bounds");
             }
-            else {
-                if (node.getNodeName().equalsIgnoreCase("line")) {
-                    int x1 = Integer.valueOf(attributeList.getNamedItem("x1").getNodeValue());
-                    int y1 = Integer.valueOf(attributeList.getNamedItem("y1").getNodeValue());
-                    int x2 = Integer.valueOf(attributeList.getNamedItem("x2").getNodeValue());
-                    int y2 = Integer.valueOf(attributeList.getNamedItem("y2").getNodeValue());
-                    if (x1 >= width || y1 >= height || x2 >= width || y2 >= height) {
-                        throw new InvalidFileFormatException("TreeTent Factory: line location out of bounds");
-                    }
-
-                    SkyscrapersCell c1 = skyscrapersBoard.getCell(x1, y1);
-                    SkyscrapersCell c2 = skyscrapersBoard.getCell(x2, y2);
-                    return new SkyscrapersLine(c1, c2);
-                }
-                else {
-                    throw new InvalidFileFormatException("TreeTent Factory: unknown puzzleElement puzzleElement");
-                }
+            if (value < 0 || value > size) {
+                throw new InvalidFileFormatException("Skyscrapers Factory: cell unknown value");
             }
+
+            SkyscrapersCell cell = new SkyscrapersCell(value, new Point(x, y), size);
+            cell.setIndex(y * size + x);
+
+            return cell;
         }
         catch (NumberFormatException e) {
-            throw new InvalidFileFormatException("TreeTent Factory: unknown value where integer expected");
+            throw new InvalidFileFormatException("Skyscrapers Factory: unknown value where integer expected");
         }
         catch (NullPointerException e) {
-            throw new InvalidFileFormatException("TreeTent Factory: could not find attribute(s)");
+            throw new InvalidFileFormatException("Skyscrapers Factory: could not find attribute(s)");
         }
     }
 
@@ -78,29 +61,15 @@ public class SkyscrapersCellFactory extends ElementFactory {
      * @return xml PuzzleElement
      */
     public org.w3c.dom.Element exportCell(Document document, PuzzleElement puzzleElement) {
-        if (puzzleElement instanceof SkyscrapersCell) {
-            org.w3c.dom.Element cellElement = document.createElement("cell");
+        org.w3c.dom.Element cellElement = document.createElement("cell");
 
-            SkyscrapersCell cell = (SkyscrapersCell) puzzleElement;
-            Point loc = cell.getLocation();
+        SkyscrapersCell cell = (SkyscrapersCell) puzzleElement;
+        Point loc = cell.getLocation();
 
-            cellElement.setAttribute("value", String.valueOf(cell.getData()));
-            cellElement.setAttribute("x", String.valueOf(loc.x));
-            cellElement.setAttribute("y", String.valueOf(loc.y));
+        cellElement.setAttribute("value", String.valueOf(cell.getData()));
+        cellElement.setAttribute("x", String.valueOf(loc.x));
+        cellElement.setAttribute("y", String.valueOf(loc.y));
 
-            return cellElement;
-        }
-        else {
-            org.w3c.dom.Element lineElement = document.createElement("line");
-
-            SkyscrapersLine line = (SkyscrapersLine) puzzleElement;
-
-            lineElement.setAttribute("x1", String.valueOf(line.getC1().getLocation().x));
-            lineElement.setAttribute("y1", String.valueOf(line.getC1().getLocation().y));
-            lineElement.setAttribute("x2", String.valueOf(line.getC2().getLocation().x));
-            lineElement.setAttribute("y2", String.valueOf(line.getC2().getLocation().y));
-
-            return lineElement;
-        }
+        return cellElement;
     }
 }
