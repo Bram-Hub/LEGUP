ae2d93ca6911cd7088535f622c2d346937611b83
zachbonagura <zachbonagura@gmail.com>
2024-02-01 23:13:38 -0500

    Implemented the BinaryCell class with the constructor, and worked on BinaryType getType() function and BinaryCell copy() function

---
+++ src/main/java/edu/rpi/legup/puzzle/binary/BinaryCell.java
@@ -0,0 +1,36 @@
+package edu.rpi.legup.puzzle.binary;
+
+import edu.rpi.legup.model.gameboard.GridCell;
+
+import java.awt.Point;
+
+public class BinaryCell extends GridCell<Integer> {
+    public BinaryCell(int valueInt, Point location) {
+        super(valueInt, location);
+    }
+
+    public BinaryType getType() {
+        switch (data) {
+            case -2:
+                return BinaryType.UNKNOWN;
+            case -1:
+                return BinaryType.ZERO;
+            case 0:
+                return BinaryType.ONE;
+            default:
+                if (data > 0) {
+                    return BinaryType.NUMBER;
+                }
+        }
+        return null;
+    }
+
+    @Override
+    public BinaryCell copy() {
+        BinaryCell copy = new BinaryCell(data, (Point) location.clone());
+        copy.setIndex(index);
+        copy.setModifiable(isModifiable);
+        copy.setGiven(isGiven);
+        return copy;
+    }
+}
