80cb273dbaf046401ded2ea016784269329dfa79
Jaden Tian <56417002+jadeandtea@users.noreply.github.com>
2024-02-07 13:59:03 -0500

    Skyscrapers puzzle editor (#720)
    * Fixed Short Truth Table case rule bug (#707)
    
    * Revert "Bugfix 549 (#682)"
    
    This reverts commit 5048ee69d958fdde9b1bb35854c56c9920068346.
    
    * Case rule test fix (#705)
    
    Co-authored-by: Chase Grajeda <76405306+Chase-Grajeda@users.noreply.github.com>
    
    * Rapid fix for STT case rules
    
    Case rules broke at some point from legacy code or merge conflict. Provided is a quick fix in CaseRule and CaseRule_Generic
    
    * Revert "Revert "Bugfix 549 (#682)"" (#706)
    
    This reverts commit e9fe310378721aa4b4fa358aa57ec44f21d086c1.
    
    ---------
    
    Co-authored-by: Chase-Grajeda <chase.grajeda@gmail.com>
    Co-authored-by: Chase Grajeda <76405306+Chase-Grajeda@users.noreply.github.com>
    
    * Implementing Puzzle Editor
    
    Removing Extraneous TreeTent Code
    Allowing setting tile number
    
    * Allow for editing clues on all axies
    
    * Remove Extraneous Code
    
    Removed functionality required for TreeTent but unnecessary for Skyscrapers
    
    * Remove Extraneous Code
    
    * Clue Tile in Editor
    
    Editor now requires selecting the clue tile to edit clues
    Added images for the tiles
    
    * Merge branch 'dev' into skyscrapersPuzzleEditor
    
    * Checkstyle Requirement
    
    * Necessary Code
    
    * Allow for test functionality
    
    ---------
    
    Co-authored-by: Charles Tian <46334090+charlestian23@users.noreply.github.com>
    Co-authored-by: Chase-Grajeda <chase.grajeda@gmail.com>
    Co-authored-by: Chase Grajeda <76405306+Chase-Grajeda@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/skyscrapers/SkyscrapersCell.java
+++ src/main/java/edu/rpi/legup/puzzle/skyscrapers/SkyscrapersCell.java
@@ -1,8 +1,10 @@
 package edu.rpi.legup.puzzle.skyscrapers;
 
+import edu.rpi.legup.model.elements.Element;
 import edu.rpi.legup.model.gameboard.GridCell;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 import static edu.rpi.legup.puzzle.skyscrapers.SkyscrapersType.convertToSkyType;
 
@@ -20,6 +22,35 @@ public class SkyscrapersCell extends GridCell<Integer> {
                 return SkyscrapersType.UNKNOWN;
             default:
                 return SkyscrapersType.Number;
+        }
+    }
+
+    @Override
+    public void setType(Element e, MouseEvent m) {
+        switch (e.getElementID()){
+            case "SKYS-UNPL-0001":
+                this.data = 0;
+                break;
+            case "SKYS-UNPL-0002":
+                switch (m.getButton()){
+                    case MouseEvent.BUTTON1:
+                        if (this.data <= 0 || this.data >= this.max) {
+                            this.data = 1;
+                        }
+                        else {
+                            this.data = this.data + 1;
+                        }
+                        break;
+                    case MouseEvent.BUTTON3:
+                        if (this.data > 1) {
+                            this.data = this.data - 1;
+                        }
+                        else {
+                            this.data = this.max;
+                        }
+                        break;
+                }
+                break;
         }
     }
 
