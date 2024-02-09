1a25c046b1020de21f2e98c1bdbecfae25c8c791
Kevin-771 <70790256+Kevin-771@users.noreply.github.com>
2024-02-09 16:47:32 -0500

    added stuff to cell and region

--- src/main/java/edu/rpi/legup/puzzle/rippleeffect/RippleEffectCell.java
+++ src/main/java/edu/rpi/legup/puzzle/rippleeffect/RippleEffectCell.java
@@ -1,10 +1,15 @@
 package edu.rpi.legup.puzzle.rippleeffect;
 
+import edu.rpi.legup.model.elements.Element;
 import edu.rpi.legup.model.gameboard.GridCell;
-import java.awt.Point;
 
-public class RippleEffectCell extends GridCell<RippleEffectCellType> {
-    public RippleEffectCell(RippleEffectCellType type, Point location) {
-        super(type, location);
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class RippleEffectCell extends GridCell<Integer>{
+    
+    public RippleEffectCell(int value, Point location) {
+        super(value, location);
     }
+
 }
---
+++ src/main/java/edu/rpi/legup/puzzle/rippleeffect/RippleEffectRegion.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.rippleeffect;
+
+import edu.rpi.legup.model.gameboard.GridRegion;
+
+public class RippleEffectRegion extends GridRegion<RippleEffectCell>{
+    public RippleEffectRegion() {
+        super();
+    }
+}
