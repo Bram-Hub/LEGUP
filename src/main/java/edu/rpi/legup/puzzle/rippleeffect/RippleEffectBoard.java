cb1bbd067c1de7a9dff1acac3fc9a19413c75b1a
Kevin-771 <70790256+Kevin-771@users.noreply.github.com>
2024-02-09 17:13:50 -0500

    added board

--- src/main/java/edu/rpi/legup/puzzle/rippleeffect/RippleEffectBoard.java
+++ src/main/java/edu/rpi/legup/puzzle/rippleeffect/RippleEffectBoard.java
@@ -2,8 +2,19 @@ package edu.rpi.legup.puzzle.rippleeffect;
 
 import edu.rpi.legup.model.gameboard.GridBoard;
 
-public class RippleEffectBoard extends GridBoard {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RippleEffectBoard extends GridBoard{
+    private List regions;
+
     public RippleEffectBoard(int width, int height) {
         super(width, height);
+        regions = new ArrayList<RippleEffectRegion>();
     }
+
+    public RippleEffectBoard(int size) {
+        this(size, size);
+    }
+    
 }
