9fa0e0205a7f744b9597135202290e1363339593
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2024-02-09 17:45:28 -0500

    Imported the black and star tiles
    ID probably needs to be changed in the future, and more assets on the way.

---
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/elements/StarTile.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.starbattle.elements;
+
+import edu.rpi.legup.model.elements.NonPlaceableElement;
+
+public class StarTile extends NonPlaceableElement {
+    public StarTile() {
+        super("LTUP-PLAC-0001", "Star Tile", "The star tile", "edu/rpi/legup/images/starbattle/star.gif");
+    }
+}
