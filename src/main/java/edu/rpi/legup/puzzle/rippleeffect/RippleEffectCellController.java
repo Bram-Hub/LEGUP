e392d6e618ee17f359954daadbd7fc0321124b62
Kevin-771 <70790256+Kevin-771@users.noreply.github.com>
2024-02-13 16:47:46 -0500

    Create RippleEffectCellController.java

---
+++ src/main/java/edu/rpi/legup/puzzle/rippleeffect/RippleEffectCellController.java
@@ -0,0 +1,24 @@
+package edu.rpi.legup.puzzle.rippleeffect;
+
+import edu.rpi.legup.controller.ElementController;
+import edu.rpi.legup.model.gameboard.PuzzleElement;
+
+import java.awt.event.MouseEvent;
+
+public class RippleEffectCellController extends ElementController {
+    @Override
+    public void changeCell(MouseEvent e, PuzzleElement data) {
+        RippleEffectCell cell = (RippleEffectCell)data;
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            if (e.isControlDown()) {
+
+            }
+            else {
+                cell.incrementCell();
+            }
+        }
+        else if (e.getButton() == MouseEvent.BUTTON3) {
+            cell.decrementCell();
+        }
+    }
+}
