1ad2347247d53afe83646401416f5ee4e3ecd735
summerhenson <summerh@gmail.com>
2024-02-13 16:37:59 -0500

    Update StarBattleCellType.java
    Fixed StarBattleCellType typos

--- src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleCellType.java
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleCellType.java
@@ -1,12 +1,12 @@
 //StarBattleCellType.java
 package edu.rpi.legup.puzzle.starbattle;
 
-public enum StarBattleType {
+public enum StarBattleCellType {
     UNKNOWN(-3), STAR(-2), BLACK(-1);
 
     public int value; 
 
-    StarBattleCell(int value) {
+    StarBattleCellType(int value) {
         this.value = value;
     }
 }
