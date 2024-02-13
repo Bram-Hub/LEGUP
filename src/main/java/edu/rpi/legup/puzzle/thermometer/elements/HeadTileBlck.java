3fec0d0b23f9e8ee8769d3d26ebb52f399bb28be
Marcus Moreno <triforcehero03@gmail.com>
2024-02-13 17:49:14 -0500

    Created element files

---
+++ src/main/java/edu/rpi/legup/puzzle/thermometer/elements/HeadTileBlck.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.thermometer.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+
+public class HeadTileBlck extends PlaceableElement {
+    public HeadTileBlck(){
+        super("Therm-PLAC-0003", "Head Tile Block", "The tile corresponding to the blocked head of a thermometer", "edu/rpi/legup/images/thermometer/HeadBlock.png");
+    }
+}
---
+++ src/main/java/edu/rpi/legup/puzzle/thermometer/elements/HeadTileEmp.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.thermometer.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+
+public class HeadTileEmp extends PlaceableElement {
+    public HeadTileEmp(){
+        super("Therm-PLAC-0001", "Head Tile Empty", "The tile corresponding to the empty head of a thermometer", "edu/rpi/legup/images/thermometer/HeadEmp.png");
+    }
+}
