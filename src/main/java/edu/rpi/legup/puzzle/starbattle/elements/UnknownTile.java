24450fd99b358f5dfb06d76933c21ad49e13d0bd
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2024-02-14 16:31:13 -0500

    Added White Tile and finished starter code for Cell
    Only Board needs to be added to get the basic puzzle running.

--- src/main/java/edu/rpi/legup/puzzle/starbattle/elements/UnknownTile.java
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/elements/UnknownTile.java
@@ -4,6 +4,6 @@ import edu.rpi.legup.model.elements.NonPlaceableElement;
 
 public class UnknownTile extends NonPlaceableElement {
     public UnknownTile() {
-        super("STBL-PLAC-0001", "Unknown Tile", "An empty tile", "edu/rpi/legup/images/starbattle/star.gif");
+        super("STBL-UNPL-0001", "Unknown Tile", "An empty tile", "edu/rpi/legup/images/starbattle/star.gif");
     }
 }
