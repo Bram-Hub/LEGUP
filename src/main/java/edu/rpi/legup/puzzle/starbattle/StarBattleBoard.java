f3b23833821404742f88b8dd073e01b5fe25ab0d
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2024-02-16 16:36:02 -0500

    Added getRow, getCol, and potential group implementation for later.
    All StarBattle boards should be square, so I deleted the width height constructor. I also added size for returning a row, col, for future rules.

--- src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleBoard.java
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleBoard.java
@@ -1,20 +1,48 @@
 package edu.rpi.legup.puzzle.starbattle;
 
+import java.util.*;
+
 import edu.rpi.legup.model.gameboard.GridBoard;
 import edu.rpi.legup.model.gameboard.PuzzleElement;
 
 public class StarBattleBoard extends GridBoard {
-    public StarBattleBoard(int width, int height) {
-        super(width, height);
-    }
+
+    private int size;
+    //private ArrayList<Integer> groupSizes;
 
     public StarBattleBoard(int size) {
         super(size, size);
+        this.size = size;
     }
 
     @Override
     public StarBattleCell getCell(int x, int y) {
         return (StarBattleCell) super.getCell(x,y);
+    }
+
+    /* 
+    public StarBattleCell getCell(int groupIndex, int x, int y) {
+        return getCell(x + (groupIndex % groupSize) * groupSize, y + (groupIndex / groupSize) * groupSize);
+    }*/
+
+    public int getSize() {
+        return size;
+    }
+
+    public Set<StarBattleCell> getRow(int rowNum) {
+        Set<StarBattleCell> row = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            row.add(getCell(i, rowNum));
+        }
+        return row;
+    }
+
+    public Set<StarBattleCell> getCol(int colNum) {
+        Set<StarBattleCell> column = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            column.add(getCell(colNum, i));
+        }
+        return column;
     }
 }
 
