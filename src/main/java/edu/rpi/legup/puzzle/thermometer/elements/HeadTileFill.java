34ace55c3143ce5ff309ac45a277d80ff64f22f5
Marcus Moreno <148644006+morenomarcus03@users.noreply.github.com>
2024-02-19 18:02:42 -0500

    Thermometer (#738)
    * Update ThermometerBoard.java
    
    * Created element files
    
    * Element/rule images

---
+++ src/main/java/edu/rpi/legup/puzzle/thermometer/elements/HeadTileFill.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.thermometer.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+
+public class HeadTileFill extends PlaceableElement {
+    public HeadTileFill(){
+        super("Therm-PLAC-0002", "Head Tile Filled", "The tile corresponding to the filled head of a thermometer", "edu/rpi/legup/images/thermometer/HeadFill.png");
+    }
+}
---
+++ src/main/java/edu/rpi/legup/puzzle/thermometer/elements/TipTileEmp.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.thermometer.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+
+public class TipTileEmp extends PlaceableElement {
+    public TipTileEmp(){
+        super("Therm-PLAC-0007", "Tip Tile Empty", "The tile corresponding to the empty tip of a thermometer", "edu/rpi/legup/images/thermometer/TipEmp.png");
+    }
+}
