9c3daa796877a9ade90c6a3cba0378ad09faca1f
summerhenson <summerh@gmail.com>
2024-02-23 15:29:26 -0500

    Direct Rule Classes
    Created skeletons of classes for direct rules.

---
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/rules/RegionsWithinRowsColumnsDirectRule.java
@@ -0,0 +1,46 @@
+package edu.rpi.legup.puzzle.starbattle.rules;
+
+import edu.rpi.legup.model.gameboard.Board;
+import edu.rpi.legup.model.gameboard.PuzzleElement;
+import edu.rpi.legup.model.rules.DirectRule;
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.starbattle.StarBattleBoard;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCell;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCellType;
+
+public class RegionsWithinRowsColumnsDirectRule extends DirectRule {
+    public RegionsWithinRowsColumnsDirectRule() {
+        super("STBL-BASC-0003",
+                "Regions Within Rows/Columns",
+                "If a number of regions is fully contained by an equal number of rows or columns, spaces of other regions in those rows or columns must be black.",
+                "INSERT IMAGE NAME HERE");
+    }
+
+    /**
+     * Checks whether the child node logically follows from the parent node
+     * at the specific puzzleElement index using this rule
+     *
+     * @param transition    transition to check
+     * @param puzzleElement equivalent puzzleElement
+     * @return null if the child node logically follow from the parent node at the specified puzzleElement,
+     * otherwise error message
+     */
+    @Override
+    public String checkRuleRawAt(TreeTransition transition, PuzzleElement puzzleElement) {
+
+        return null;
+    }
+
+    /**
+     * Creates a transition {@link Board} that has this rule applied to it using the {@link TreeNode}.
+     *
+     * @param node tree node used to create default transition board
+     * @return default board or null if this rule cannot be applied to this tree node
+     */
+    @Override
+    public Board getDefaultBoard(TreeNode node) {
+
+        return null;
+    }
+}
---
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/rules/SurroundStarDirectRule.java
@@ -0,0 +1,48 @@
+package edu.rpi.legup.puzzle.starbattle.rules;
+
+import edu.rpi.legup.model.gameboard.Board;
+import edu.rpi.legup.model.gameboard.PuzzleElement;
+import edu.rpi.legup.model.rules.DirectRule;
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.starbattle.StarBattleBoard;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCell;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCellType;
+
+public class SurroundStarDirectRule extends DirectRule {
+
+    public SurroundStarDirectRule() {
+        super("STBL-BASC-0005",
+                "Surround Star",
+                "Any space adjacent to a star must be black.",
+                "INSERT IMAGE NAME HERE");
+    }
+
+    /**
+     * Checks whether the child node logically follows from the parent node
+     * at the specific puzzleElement index using this rule
+     *
+     * @param transition    transition to check
+     * @param puzzleElement equivalent puzzleElement
+     * @return null if the child node logically follow from the parent node at the specified puzzleElement,
+     * otherwise error message
+     */
+    @Override
+    public String checkRuleRawAt(TreeTransition transition, PuzzleElement puzzleElement) {
+
+        return null;
+    }
+
+    /**
+     * Creates a transition {@link Board} that has this rule applied to it using the {@link TreeNode}.
+     *
+     * @param node tree node used to create default transition board
+     * @return default board or null if this rule cannot be applied to this tree node
+     */
+    @Override
+    public Board getDefaultBoard(TreeNode node) {
+
+        return null;
+    }
+}
+
