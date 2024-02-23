c46c46e6d0713a2464330c751f681696f464a0a2
Brandon McCusker <bmccusker56@gmail.com>
2024-02-23 17:16:20 -0500

    Deleted false config file, began rule implementation, began fixing puzzle board visual

--- src/main/java/edu/rpi/legup/puzzle/binary/rules/CompleteRowColumnDirectRule.java
+++ src/main/java/edu/rpi/legup/puzzle/binary/rules/CompleteRowColumnDirectRule.java
@@ -10,6 +10,10 @@ import edu.rpi.legup.puzzle.binary.BinaryBoard;
 import edu.rpi.legup.puzzle.binary.BinaryCell;
 import edu.rpi.legup.puzzle.binary.BinaryType;
 
+import java.util.Set;
+import java.util.HashSet;
+
+
 public class CompleteRowColumnDirectRule extends DirectRule {
 
     public CompleteRowColumnDirectRule() {
@@ -18,9 +22,52 @@ public class CompleteRowColumnDirectRule extends DirectRule {
                 "If a row/column of length n contains n/2 of a single value, the remaining cells must contain the other value",
                 "FILL IN WITH IMAGE");
     }
-
+    /**
+     * Checks whether the child node logically follows from the parent node
+     * at the specific puzzleElement index using this rule
+     *
+     * @param transition    transition to check
+     * @param puzzleElement equivalent puzzleElement
+     * @return null if the child node logically follow from the parent node at the specified puzzleElement,
+     * otherwise error message
+     */
     @Override
     public String checkRuleRawAt(TreeTransition transition, PuzzleElement puzzleElement) {
+        BinaryBoard initialBoard = (BinaryBoard) transition.getParents().get(0).getBoard();
+        BinaryBoard finalBoard = (BinaryBoard) transition.getBoard();
+        int elementIndex = puzzleElement.getIndex();
+        int boardDim = initialBoard.getWidth();
+        int elementRow = elementIndex / boardDim;
+        int elementCol = elementIndex % boardDim;
+        Set<BinaryCell> curRow = initialBoard.getRow(elementRow);
+        Set<BinaryCell> curCol = initialBoard.getCol(elementCol); 
+     
+            int numColZeros = 0;
+            int numColOnes = 0;
+
+        for (int i = 0; i < boardDim; i++) {
+            BinaryCell cell = initialBoard.getCell(i, elementCol);
+            if(cell.getData() == 1)
+                numColOnes ++;
+            else if(cell.getData() == 0)
+                numColZeros ++; 
+        }
+            int numRowZeros = 0;
+            int numRowOnes = 0;
+
+        for (int i = 0; i < boardDim; i++) {
+            BinaryCell cell = initialBoard.getCell(elementRow, i);
+            if(cell.getData() == 1)
+                numRowOnes ++;
+            else if(cell.getData() == 0)
+                numRowZeros ++; 
+        }
+        if (numColOnes + numColZeros != boardDim) {
+            return super.getInvalidUseOfRuleMessage() + ": The column for the specificed element is not complete";
+        }
+         if (numRowOnes + numRowZeros != boardDim) {
+            return super.getInvalidUseOfRuleMessage() + ": The row for the specified element is not complete";
+        }
         return null;
     }
 
