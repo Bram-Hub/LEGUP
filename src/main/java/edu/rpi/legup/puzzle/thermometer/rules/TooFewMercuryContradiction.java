0e0590df65b232a2fcc1ff40860369ced3f8ccab
Marcus Moreno <triforcehero03@gmail.com>
2024-02-23 22:01:31 -0500

    Too few mercury contradiction rule

--- src/main/java/edu/rpi/legup/puzzle/thermometer/rules/TooFewMercuryContradiction.java
+++ src/main/java/edu/rpi/legup/puzzle/thermometer/rules/TooFewMercuryContradiction.java
@@ -7,31 +7,31 @@ import edu.rpi.legup.puzzle.thermometer.ThermometerBoard;
 import edu.rpi.legup.puzzle.thermometer.ThermometerCell;
 import edu.rpi.legup.puzzle.thermometer.ThermometerFill;
 
-public class TooManyMercuryContradiction extends ContradictionRule{
+public class TooFewMercuryContradiction extends ContradictionRule{
 
-    private final String Invalid_Use_Message = "Mercury does not exceed limit";
+    private final String Invalid_Use_Message = "Mercury can still reach limit";
 
 
-    public TooManyMercuryContradiction(){
+    public TooFewMercuryContradiction(){
         super("THERM-CONT-0002", "Too Many Mercury", "More mercury in column/row than target", "edu/rpi/legup/images/thermometer/TooManyMercury.png");
     }
 
     @Override
-    //Checks if mercury tiles in column or row of input element exceed limit.
+    //Checks if row or column of input element has too many blocked tiles
     public String checkContradictionAt(Board board, PuzzleElement puzzleElement) {
         ThermometerBoard grid = (ThermometerBoard) board;
         ThermometerCell cell = (ThermometerCell) grid.getPuzzleElement(puzzleElement);
-        int filled = 0;
+        int blocked = 0;
         for(int i = 0; i < grid.getHeight(cell.getLocation().getX()); i++){
-            if(grid.getCell((int)cell.getLocation().getX(), i).getFill() == ThermometerFill.FILLED){ filled++;}
+            if(grid.getCell((int)cell.getLocation().getX(), i).getFill() == ThermometerFill.BLOCKED){ blocked++;}
         }
-        if(grid.getRowNumber(cell.getLocation().getX()) > filled) return null;
+        if(grid.getRowNumber(cell.getLocation().getX()) > blocked) return null;
 
-        filled = 0;
+        blocked = 0;
         for(int i = 0; i < grid.getColumn(cell.getLocation().getY()); i++){
-            if(grid.getCell(i, (int)cell.getLocation().getY()).getFill() == ThermometerFill.FILLED){ filled++;}
+            if(grid.getCell(i, (int)cell.getLocation().getY()).getFill() == ThermometerFill.BLOCKED){ blocked++;}
         }
-        if(grid.getColumnNumber(cell.getLocation().getY()) > filled) return null;
+        if(grid.getColumnNumber(cell.getLocation().getY()) > blocked) return null;
 
         return Invalid_Use_Message;
     }
