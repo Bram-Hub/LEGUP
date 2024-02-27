e677a6eec68aa48ebe8fe121923f979b227baf2f
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2024-02-27 16:50:36 -0500

    Adding Region files

---
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleRegion.java
@@ -0,0 +1,9 @@
+package edu.rpi.legup.puzzle.starbattle;
+
+import edu.rpi.legup.model.gameboard.GridRegion;
+
+public class StarBattleRegion extends GridRegion<StarBattleCell>{
+    public StarBattleRegion() {
+        super();
+    }
+}
