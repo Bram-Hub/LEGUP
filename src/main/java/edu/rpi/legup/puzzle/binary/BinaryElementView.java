f33342931c085630a466bc1f7d487680e463755a
Brandon McCusker <bmccusker56@gmail.com>
2024-03-12 17:04:48 -0400

    Altered given cells color and fixed rules ref

--- src/main/java/edu/rpi/legup/puzzle/binary/BinaryElementView.java
+++ src/main/java/edu/rpi/legup/puzzle/binary/BinaryElementView.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 public class BinaryElementView extends GridElementView {
 
-    private static final Font FONT = new Font("TimesRoman", Font.BOLD, 16);
+    private static final Font FONT = new Font("TimesRoman", Font.BOLD, 17);
     private static final Color FONT_COLOR = Color.BLACK;
 
     public BinaryElementView(BinaryCell cell) {
@@ -21,6 +21,51 @@ public class BinaryElementView extends GridElementView {
     @Override
     public BinaryCell getPuzzleElement() {
         return (BinaryCell) super.getPuzzleElement();
+    
+    }@Override
+    public void drawGiven(Graphics2D graphics2D) {
+            BinaryCell cell = (BinaryCell) puzzleElement;
+        BinaryType type = cell.getType();
+        if (type == BinaryType.ZERO) {
+            graphics2D.setStroke(new BasicStroke(1));
+            graphics2D.setColor(Color.LIGHT_GRAY);
+            graphics2D.fillRect(location.x, location.y, size.width, size.height);
+            graphics2D.setColor(Color.BLACK);
+            graphics2D.drawRect(location.x, location.y, size.width, size.height);
+            graphics2D.setColor(FONT_COLOR);             
+            graphics2D.setFont(FONT);             
+            FontMetrics metrics = graphics2D.getFontMetrics(FONT);             
+            String value = String.valueOf(puzzleElement.getData());             
+            int xText = location.x + (size.width - metrics.stringWidth(value)) / 2;           
+            int yText = location.y + ((size.height - metrics.getHeight()) / 2) + metrics.getAscent();
+            graphics2D.drawString(String.valueOf(puzzleElement.getData()), xText, yText);
+        }
+        else {
+            if (type == BinaryType.ONE) {
+                graphics2D.setStroke(new BasicStroke(1));
+                graphics2D.setColor(Color.LIGHT_GRAY);
+                graphics2D.fillRect(location.x, location.y, size.width, size.height);
+                graphics2D.setColor(Color.BLACK);
+                graphics2D.drawRect(location.x, location.y, size.width, size.height);
+                graphics2D.setColor(FONT_COLOR);             
+                graphics2D.setFont(FONT);             
+                FontMetrics metrics = graphics2D.getFontMetrics(FONT);             
+                String value = String.valueOf(puzzleElement.getData());             
+                int xText = location.x + (size.width - metrics.stringWidth(value)) / 2;           
+                int yText = location.y + ((size.height - metrics.getHeight()) / 2) + metrics.getAscent();
+                graphics2D.drawString(String.valueOf(puzzleElement.getData()), xText, yText);
+    
+            }
+            else {
+                if (type == BinaryType.UNKNOWN) {
+                    graphics2D.setStroke(new BasicStroke(0));
+                    graphics2D.setColor(Color.WHITE);
+                    graphics2D.fillRect(location.x, location.y, size.width, size.height);
+                    graphics2D.setColor(Color.BLACK);
+                    graphics2D.drawRect(location.x, location.y, size.width, size.height);
+                }
+            }
+        }
     }
 
     @Override
@@ -60,7 +105,7 @@ public class BinaryElementView extends GridElementView {
             else {
                 if (type == BinaryType.UNKNOWN) {
                     graphics2D.setStroke(new BasicStroke(0));
-                    graphics2D.setColor(Color.LIGHT_GRAY);
+                    graphics2D.setColor(Color.WHITE);
                     graphics2D.fillRect(location.x, location.y, size.width, size.height);
                     graphics2D.setColor(Color.BLACK);
                     graphics2D.drawRect(location.x, location.y, size.width, size.height);
