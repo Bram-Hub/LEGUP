cb667f893d64c4883e107ef8c659eadb3d3a0c9b
EggyMath <55564321+EggyMath@users.noreply.github.com>
2024-03-12 17:22:13 -0400

    Added two new versions of the Possible Cell For Number case rule
    Added and bug tested two alternate versions of the region based Possible Cells For Number case rule.

--- src/main/java/edu/rpi/legup/puzzle/sudoku/rules/PossibleCellsForNumberColumnCaseRule.java
+++ src/main/java/edu/rpi/legup/puzzle/sudoku/rules/PossibleCellsForNumberColumnCaseRule.java
@@ -5,20 +5,23 @@ import edu.rpi.legup.model.gameboard.CaseBoard;
 import edu.rpi.legup.model.gameboard.PuzzleElement;
 import edu.rpi.legup.model.rules.CaseRule;
 import edu.rpi.legup.model.tree.TreeTransition;
-import edu.rpi.legup.puzzle.sudoku.*;
+import edu.rpi.legup.puzzle.sudoku.GroupType;
+import edu.rpi.legup.puzzle.sudoku.ModelSudokuBoard;
+import edu.rpi.legup.puzzle.sudoku.SudokuBoard;
+import edu.rpi.legup.puzzle.sudoku.SudokuCell;
 
 import java.util.ArrayList;
 import java.util.Set;
 
-public class PossibleCellsForNumberRegionCaseRule extends CaseRule {
+public class PossibleCellsForNumberColumnCaseRule extends CaseRule {
 
     //Board math for translating indexes to numbers
     private ModelSudokuBoard model = new ModelSudokuBoard();
 
     //Old board for caseBoard reference
     private SudokuBoard lagBoard;
-    public PossibleCellsForNumberRegionCaseRule() {
-        super("SUDO-CASE-0002", "Possible Cells for Number - Region",
+    public PossibleCellsForNumberColumnCaseRule() {
+        super("SUDO-CASE-0004", "Possible Cells for Number - Column",
                 "An empty cell has a limited set of possible numbers that can fill it.",
                 "edu/rpi/legup/images/sudoku/possible_cells_number.png");
     }
@@ -54,7 +57,7 @@ public class PossibleCellsForNumberRegionCaseRule extends CaseRule {
         lagBoard = (SudokuBoard) sudokuBoard.copy();
         CaseBoard caseBoard = new CaseBoard(sudokuBoard, this);
         for (PuzzleElement puzzleElement : sudokuBoard.getPuzzleElements()) {
-            puzzleElement.setData(model.getModelRegionNumbers(puzzleElement.getIndex()));
+            puzzleElement.setData(model.getModelColumnNumbers(puzzleElement.getIndex()));
             caseBoard.addPickableElement(puzzleElement);
         }
         return caseBoard;
@@ -69,7 +72,7 @@ public class PossibleCellsForNumberRegionCaseRule extends CaseRule {
      */
     @Override
     public ArrayList<Board> getCases(Board board, PuzzleElement puzzleElement) {
-        return getCases(board, puzzleElement, 1, GroupType.REGION);
+        return getCases(board, puzzleElement, 1, GroupType.COLUMN);
     }
 
     /**
@@ -86,12 +89,12 @@ public class PossibleCellsForNumberRegionCaseRule extends CaseRule {
         SudokuBoard sudokuBoard = lagBoard;
         SudokuCell sourceCell = (SudokuCell) puzzleElement;
 
-        Set<SudokuCell> group = sudokuBoard.getRegion(sourceCell.getGroupIndex());
+        Set<SudokuCell> group = sudokuBoard.getCol(sourceCell.getLocation().x);
         for (SudokuCell cell : group){
             if(cell.getData() == 0){
                 Board newCase = sudokuBoard.copy();
                 PuzzleElement element = newCase.getPuzzleElement(cell);
-                element.setData(model.getModelRegionNumbers(sourceCell.getIndex()));
+                element.setData(model.getModelColumnNumbers(sourceCell.getIndex()));
                 newCase.addModifiedData(element);
                 cases.add(newCase);
             }
