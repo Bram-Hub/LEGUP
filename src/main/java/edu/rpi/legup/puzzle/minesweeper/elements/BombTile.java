b121d28974743d0a27f7e77fd352da72e95419fd
FisherLuba <145061313+FisherLuba@users.noreply.github.com>
2024-03-12 17:25:36 -0400

    Added reference sheet for Minesweeper tiles for puzzle editor

--- src/main/java/edu/rpi/legup/puzzle/minesweeper/elements/BombTile.java
+++ src/main/java/edu/rpi/legup/puzzle/minesweeper/elements/BombTile.java
@@ -1,8 +1,8 @@
 package edu.rpi.legup.puzzle.minesweeper.elements;
 
 import edu.rpi.legup.model.elements.NonPlaceableElement;
-public class Bomb extends NonPlaceableElement{
-    public Bomb() {
+public class BombTile extends NonPlaceableElement{
+    public BombTile() {
         super("MINE-UNPL-0001", "Bomb", "A bomb", "edu/rpi/legup/images/minesweeper/tiles/Bomb.png");
     }
 }
---
+++ src/main/java/edu/rpi/legup/puzzle/minesweeper/elements/FlagTile.java
@@ -0,0 +1,8 @@
+package edu.rpi.legup.puzzle.minesweeper.elements;
+
+import edu.rpi.legup.model.elements.PlaceableElement;
+public class FlagTile extends PlaceableElement{
+    public FlagTile() {
+        super("MINE-PLAC-0001", "Flag", "The flag", "edu/rpi/legup/images/nurikabe/tiles/BlackTile.png");
+    }
+}
