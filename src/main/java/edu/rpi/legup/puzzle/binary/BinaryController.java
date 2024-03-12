1f5fd80bd6baea989c944e5475d1f3746a5a39f9
Brandon McCusker <bmccusker56@gmail.com>
2024-03-12 16:32:34 -0400

    All user board interactions fiex to be correct

--- src/main/java/edu/rpi/legup/puzzle/binary/BinaryController.java
+++ src/main/java/edu/rpi/legup/puzzle/binary/BinaryController.java
@@ -15,27 +15,27 @@ public class BinaryController extends ElementController {
                 this.boardView.getSelectionPopupMenu().show(boardView, this.boardView.getCanvas().getX() + e.getX(), this.boardView.getCanvas().getY() + e.getY());
             }
             else {
-                if (cell.getData() == -2) {
-                    data.setData(0);
+                if (cell.getData() == 0) {
+                    data.setData(1);
                 }
                 else {
-                    if (cell.getData() == 0) {
-                        data.setData(-1);
+                    if (cell.getData() == 1) {
+                        data.setData(2);
                     }
                     else {
-                        data.setData(-2);
+                        data.setData(0);
                     }
                 }
             }
         }
         else {
             if (e.getButton() == MouseEvent.BUTTON3) {
-                if (cell.getData() == -2) {
-                    data.setData(-1);
+                if (cell.getData() == 0) {
+                    data.setData(1);
                 }
                 else {
-                    if (cell.getData() == 0) {
-                        data.setData(-2);
+                    if (cell.getData() == 1) {
+                        data.setData(2);
                     }
                     else {
                         data.setData(0);
