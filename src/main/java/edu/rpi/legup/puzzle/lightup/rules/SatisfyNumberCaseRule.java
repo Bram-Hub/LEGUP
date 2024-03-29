af41ba153d537e96022182d2a2c2dc4dbcc5f944
charlestian23 <charles.tian23@gmail.com>
2024-03-29 11:22:15 -0400

    Adding bad formatting for testing

--- src/main/java/edu/rpi/legup/puzzle/lightup/rules/SatisfyNumberCaseRule.java
+++ src/main/java/edu/rpi/legup/puzzle/lightup/rules/SatisfyNumberCaseRule.java
@@ -48,7 +48,8 @@ public class SatisfyNumberCaseRule extends CaseRule {
     @Override
     public ArrayList<Board> getCases(Board board, PuzzleElement puzzleElement) {
         LightUpBoard lightUpBoard = (LightUpBoard) board;
-        LightUpCell cell = (LightUpCell) puzzleElement;
+        LightUpCell cell
+                = (LightUpCell) puzzleElement;
         Point loc = cell.getLocation();
 
         List<LightUpCell> openSpots = new ArrayList<>();
