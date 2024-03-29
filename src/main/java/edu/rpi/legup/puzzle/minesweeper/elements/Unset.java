5a520704e23a50615c42dd9ecbb31d73af4538b6
vockek <vockek@rpi.edu>
2024-03-29 17:16:18 -0400

    Delete Unset (replaced by UnsetTile)

--- src/main/java/edu/rpi/legup/puzzle/minesweeper/elements/Unset.java
+++
@@ -1,9 +0,0 @@
-package edu.rpi.legup.puzzle.minesweeper.elements;
-
-import edu.rpi.legup.model.elements.NonPlaceableElement;
-
-public class Unset extends NonPlaceableElement {
-    public Unset() {
-        super("MINE-UNPL-0002", "Unset", "A blank tile", "edu/rpi/legup/images/fillapix/tiles/UnknownTile.png");
-    }
-}
