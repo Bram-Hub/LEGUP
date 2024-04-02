69a011f6b2d10edeb6e3b30daa02e693ca585643
Bram van Heuveln <56417002+jadeandtea@users.noreply.github.com>
2024-04-02 17:53:59 -0400

    TreeForTent tests and TentForTree tests

--- src/test/java/puzzles/treetent/rules/TentForTreeDirectRuleTest.java
+++ src/test/java/puzzles/treetent/rules/TentForTreeDirectRuleTest.java
@@ -32,7 +32,7 @@ public class TentForTreeDirectRuleTest {
   public void TentForTreeTestOneTreeOneTentTest() throws InvalidFileFormatException {
 
     TestUtilities.importTestBoard(
-            "puzzles/treetent/rules/TentForTreeDirectRule/TentForTreeOneTreeOneTent",
+            "puzzles/treetent/rules/TentForTreeDirectRule/OneTreeOneTent",
           treetent);
 
     TreeNode rootNode = treetent.getTree().getRootNode();
@@ -61,14 +61,18 @@ public class TentForTreeDirectRuleTest {
   }
 
     /***
-     * @throws InvalidFileFormatException Test to check that a line connecting a tree and tent
-     *    while there are multiple tents around the tree fails
+     * @throws InvalidFileFormatException Test to check that a line connecting a tent to a tree
+     *    while there are multiple trees around the tent works
+     *
+     *    <p>3x3 board with trees at (1, 0) and (1, 2) and a tent at (1, 1). Creating a line
+     *    from (1, 0) to (1, 1) works because there is only one tent adjacent to the tree
+     *    at (1, 0)
      */
     @Test
-    public void TentForTreeOneTreeTwoTentTest() throws InvalidFileFormatException {
+    public void TentForTreeArbitraryTreeTest() throws InvalidFileFormatException {
 
         TestUtilities.importTestBoard(
-                "puzzles/treetent/rules/TentForTreeDirectRule/TentForTreeOneTreeTwoTent",
+                "puzzles/treetent/rules/TentForTreeDirectRule/ArbitraryTree",
                 treetent);
 
         TreeNode rootNode = treetent.getTree().getRootNode();
@@ -85,24 +89,21 @@ public class TentForTreeDirectRuleTest {
         board.getLines().add(line);
 
         Assert.assertNull(RULE.checkRule(transition));
-
-        ArrayList<TreeTentLine> lines = board.getLines();
-        for (TreeTentLine l : lines) {
-            Assert.assertNotNull(RULE.checkRuleAt(transition, l));
-        }
     }
 
     /***
      * @throws InvalidFileFormatException Test to check if attempting to connect a tree to
      *  an already connected tent fails
+     *
+     *  <p>3x3 Board with Trees at (1, 0) and (1, 2) and a Tent at (1, 1)
+     *  A Preexisting line connects the tent at (1, 0) and the central tent
+     *  Therefore, the tree at (1, 2) does not have a valid tent adjacent to it
      */
-    //TODO
-    // Currently Cannot Create a board with a pre-existing line.
     @Test
-    public void TentForTreeOneTreeConnectedTent() throws InvalidFileFormatException {
+    public void TentForTreeConnectedTent() throws InvalidFileFormatException {
 
         TestUtilities.importTestBoard(
-                "puzzles/treetent/rules/TentForTreeDirectRule/TentForTreeOneTreeTwoTent",
+                "puzzles/treetent/rules/TentForTreeDirectRule/ArbitraryTree",
                 treetent);
 
         TreeNode rootNode = treetent.getTree().getRootNode();
@@ -124,5 +125,34 @@ public class TentForTreeDirectRuleTest {
         for (TreeTentLine l : lines) {
             Assert.assertNotNull(RULE.checkRuleAt(transition, l));
         }
+    }
+
+    /***
+     * @throws InvalidFileFormatException Test to check if attempting to connect a tree to
+     *  a tent when there are two possibilities fails
+     *
+     *  <p>3x3 Board with Tree at (1, 1) and Tents at (1, 0) and (1, 2)
+     *  The central tree has two possible tents to connect to, so the rule should fail
+     */
+    @Test
+    public void TentForTreeOneTreeTwoAdjacentTent() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard(
+                "puzzles/treetent/rules/TentForTreeDirectRule/OneTreeTwoAdjacentTent",
+                treetent);
+
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 2);
+        TreeTentCell cell2 = board.getCell(1, 1);
+        TreeTentLine line = new TreeTentLine(cell1, cell2);
+
+        board.addModifiedData(line);
+        board.getLines().add(line);
+
+        Assert.assertNotNull(RULE.checkRule(transition));
     }
 }
--- src/test/java/puzzles/treetent/rules/TreeForTentDirectRuleTest.java
+++ src/test/java/puzzles/treetent/rules/TreeForTentDirectRuleTest.java
@@ -1,14 +1,154 @@
 package puzzles.treetent.rules;
 
-// This feature is no longer supported
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.treetent.TreeTent;
+import edu.rpi.legup.puzzle.treetent.TreeTentBoard;
+import edu.rpi.legup.puzzle.treetent.TreeTentCell;
+import edu.rpi.legup.puzzle.treetent.TreeTentLine;
+import edu.rpi.legup.puzzle.treetent.rules.TreeForTentDirectRule;
+import edu.rpi.legup.save.InvalidFileFormatException;
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class TreeForTentDirectRuleTest {
 
-    //    private static final TreeForTentBasicRule RULE = new TreeForTentBasicRule();
-    //    private static TreeTent treetent;
+    private static final TreeForTentDirectRule RULE = new TreeForTentDirectRule();
+    private static TreeTent treetent;
 
-    //    @BeforeClass
-    //    public static void setUp() {
-    //        MockGameBoardFacade.getInstance();
-    //        treetent = new TreeTent();
-    //    }
+    @BeforeClass
+    public static void setUp() {
+        MockGameBoardFacade.getInstance();
+        treetent = new TreeTent();
+    }
+
+  /***
+   * @throws InvalidFileFormatException Test to check that a line connecting a tree and tent
+   *    that are only adjacent to each other is valid
+   *
+   *    <p>3x3 Board with Tree at (1, 0) and a Tent at (1, 1)
+   */
+  @Test
+  public void TreeForTentTestOneTreeOneTentTest() throws InvalidFileFormatException {
+
+        TestUtilities.importTestBoard(
+                "puzzles/treetent/rules/TreeForTentDirectRule/OneTentOneTree",
+                treetent);
+
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 0);
+        TreeTentCell cell2 = board.getCell(1, 1);
+        TreeTentLine line = new TreeTentLine(cell1, cell2);
+
+        board.addModifiedData(line);
+        board.getLines().add(line);
+
+        Assert.assertNull(RULE.checkRule(transition));
+    }
+
+    /***
+     * @throws InvalidFileFormatException Test to check that a line connecting a tree to a tent
+     *    while there are multiple tents around the tree works
+     *
+     *    <p>3x3 board with tents at (1, 0) and (1, 2) and a tree at (1, 1). Creating a line
+     *    from (1, 0) to (1, 1) works because there is only one tree adjacent to the tent
+     *    at (1, 0)
+     */
+    @Test
+    public void TentForTreeArbitraryTreeTest() throws InvalidFileFormatException {
+
+        TestUtilities.importTestBoard(
+                "puzzles/treetent/rules/TreeForTentDirectRule/ArbitraryTent",
+                treetent);
+
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 0);
+        TreeTentCell cell2 = board.getCell(1, 1);
+        TreeTentLine line = new TreeTentLine(cell1, cell2);
+
+        board.addModifiedData(line);
+        board.getLines().add(line);
+
+        Assert.assertNull(RULE.checkRule(transition));
+    }
+
+    /***
+     * @throws InvalidFileFormatException Test to check if attempting to connect a tent to
+     *  an already connected tree fails
+     *
+     *  <p>3x3 Board with Tents at (1, 0) and (1, 2) and a Tree at (1, 1)
+     *  A Preexisting line connects the tent at (1, 0) and the central tree
+     *  Therefore, the tent at (1, 2) does not have a valid tree adjacent to it
+     */
+    @Test
+    public void TentForTreeConnectedTent() throws InvalidFileFormatException {
+
+        TestUtilities.importTestBoard(
+                "puzzles/treetent/rules/TreeForTentDirectRule/ConnectedTree",
+                treetent);
+
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 2);
+        TreeTentCell cell2 = board.getCell(1, 1);
+        TreeTentLine line = new TreeTentLine(cell1, cell2);
+
+        board.addModifiedData(line);
+        board.getLines().add(line);
+
+        Assert.assertNull(RULE.checkRule(transition));
+
+        ArrayList<TreeTentLine> lines = board.getLines();
+        for (TreeTentLine l : lines) {
+            Assert.assertNotNull(RULE.checkRuleAt(transition, l));
+        }
+    }
+
+    /***
+     * @throws InvalidFileFormatException Test to check if attempting to connect a tent to
+     *  a tree when there are two trees fails
+     *
+     *  <p>3x3 Board with Tent at (1, 1) and Trees at (1, 0) and (1, 2)
+     *  The central tent has two possible trees to connect to, so the rule should fail
+     */
+    @Test
+    public void TentForTreeOneTreeTwoAdjacentTent() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard(
+                "puzzles/treetent/rules/TreeForTentDirectRule/OneTentTwoAdjacentTrees",
+                treetent);
+
+        TreeNode rootNode = treetent.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        TreeTentBoard board = (TreeTentBoard) transition.getBoard();
+
+        TreeTentCell cell1 = board.getCell(1, 2);
+        TreeTentCell cell2 = board.getCell(1, 1);
+        TreeTentLine line = new TreeTentLine(cell1, cell2);
+
+        board.addModifiedData(line);
+        board.getLines().add(line);
+
+        Assert.assertNotNull(RULE.checkRule(transition));
+    }
 }
