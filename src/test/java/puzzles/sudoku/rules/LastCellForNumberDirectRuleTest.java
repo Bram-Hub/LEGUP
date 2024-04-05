fd90f2da2db741561ae2c3e2ca835d2733af1e0e
kchiu1 <152306707+kchiu1@users.noreply.github.com>
2024-04-05 16:34:45 -0400

    Deleted fake tests and added a real test
    Fake tests was bad

---
+++ src/test/java/puzzles/sudoku/rules/LastCellForNumberDirectRuleTest.java
@@ -0,0 +1,10 @@
+package puzzles.sudoku.rules;
+
+import static org.junit.Assert.*;
+
+public class LastCellForNumberDirectRuleTest {
+    @test
+    public LastCellForNumberDirectRuleTest() {
+        assertTrue(true);
+    }
+}
