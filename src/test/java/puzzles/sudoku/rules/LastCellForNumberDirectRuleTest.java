ceab9056aed9a4f49794fd2aad09bb559f3f9b76
kchiu1 <152306707+kchiu1@users.noreply.github.com>
2024-04-05 17:42:07 -0400

    Modified tests, made sample test

--- src/test/java/puzzles/sudoku/rules/LastCellForNumberDirectRuleTest.java
+++ src/test/java/puzzles/sudoku/rules/LastCellForNumberDirectRuleTest.java
@@ -1,20 +1,27 @@
 package puzzles.sudoku.rules;
 
 import edu.rpi.legup.puzzle.nurikabe.Nurikabe;
+import edu.rpi.legup.save.InvalidFileFormatException;
 import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import edu.rpi.legup.puzzle.sudoku.Sudoku;
+import edu.rpi.legup.puzzle.sudoku.rules.LastCellForNumberDirectRule;
 
 import static org.junit.Assert.*;
 
 public class LastCellForNumberDirectRuleTest {
-    private Sudoku sudoku;
+    private static Sudoku sudoku;
+    @BeforeClass
     public static void setUp() {
         MockGameBoardFacade.getInstance();
         sudoku = new Sudoku();
     }
     @Test
-    public void LastCellForNumberDirectRuleTest() {
+    public void LastCellForNumberDirectRuleTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/sudoku/rules/LastCellForNumberRule/TestBoard", sudoku);
         assertTrue(true);
+
     }
 }
