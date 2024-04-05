999fa1780ad12f8ecbe0912d3da285b7b7c902ee
summerhenson <summerh@gmail.com>
2024-04-05 16:42:27 -0400

    Made small test board

---
+++ src/test/java/puzzles/starbattle/rules/BlackoutDirectRuleTest.java
@@ -0,0 +1,19 @@
+package puzzles.starbattle.rules;
+
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.rpi.legup.puzzle.starbattle.StarBattle;
+import edu.rpi.legup.puzzle.starbattle.StarBattleBoard;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCell;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCellType;
+import edu.rpi.legup.puzzle.starbattle.rules.BlackoutDirectRule;
+import edu.rpi.legup.save.InvalidFileFormatException;
+
+public class BlackoutDirectRuleTest {
+}
