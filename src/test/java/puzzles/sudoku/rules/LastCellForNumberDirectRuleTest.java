c5653ca5c6a61f88c5b39e5b8c4b01bbdae4293c
kchiu1 <152306707+kchiu1@users.noreply.github.com>
2024-04-05 16:47:31 -0400

    Update LastCellForNumberDirectRuleTest.java

--- src/test/java/puzzles/sudoku/rules/LastCellForNumberDirectRuleTest.java
+++ src/test/java/puzzles/sudoku/rules/LastCellForNumberDirectRuleTest.java
@@ -1,10 +1,18 @@
 package puzzles.sudoku.rules;
 
+import edu.rpi.legup.puzzle.nurikabe.Nurikabe;
+import legup.MockGameBoardFacade;
 import org.junit.Test;
+import edu.rpi.legup.puzzle.sudoku.Sudoku;
 
 import static org.junit.Assert.*;
 
 public class LastCellForNumberDirectRuleTest {
+    private Sudoku sudoku;
+    public static void setUp() {
+        MockGameBoardFacade.getInstance();
+        sudoku = new Sudoku();
+    }
     @Test
     public void LastCellForNumberDirectRuleTest() {
         assertTrue(true);
