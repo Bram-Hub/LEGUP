70de0d76fb8867c41655a537622e155bb74cb2a1
EggyMath <55564321+EggyMath@users.noreply.github.com>
2024-04-09 17:15:23 -0400

    More Tests for RepeatedNumbers
    Added another test for RepeatedNumbers contradiction rule and fixed previous error.

--- src/test/java/puzzles/sudoku/rules/RepeatedNumberContradictionRuleTest.java
+++ src/test/java/puzzles/sudoku/rules/RepeatedNumberContradictionRuleTest.java
@@ -5,7 +5,6 @@ import edu.rpi.legup.model.tree.TreeTransition;
 import edu.rpi.legup.puzzle.sudoku.Sudoku;
 import edu.rpi.legup.puzzle.sudoku.SudokuBoard;
 import edu.rpi.legup.puzzle.sudoku.SudokuCell;
-import edu.rpi.legup.puzzle.sudoku.rules.LastNumberForCellDirectRule;
 import edu.rpi.legup.puzzle.sudoku.rules.RepeatedNumberContradictionRule;
 import edu.rpi.legup.save.InvalidFileFormatException;
 import legup.MockGameBoardFacade;
@@ -25,7 +24,7 @@ public class RepeatedNumberContradictionRuleTest {
     @Test
     public void RepeatedNumberContradictionRule_GlobalTest() throws InvalidFileFormatException {
         //Import board and create transition
-        TestUtilities.importTestBoard("puzzles/sudoku/rules/RepeatedNumberContradictionRule/BlankBoard", sudoku);
+        TestUtilities.importTestBoard("puzzles/sudoku/rules/RepeatedNumberContradictionRule/BlankBoard7", sudoku);
         TreeNode rootNode = sudoku.getTree().getRootNode();
         TreeTransition transition = rootNode.getChildren().get(0);
         transition.setRule(RULE);
@@ -37,13 +36,41 @@ public class RepeatedNumberContradictionRuleTest {
             //Set cell
             int x = i / 9;
             int y = i % 9;
-            if(x == 0 && y == 0)
+            if(x == 0 && y == 0) {
                 continue;
+            }
             SudokuCell cell = board.getCell(x, y);
             cell.setData(7);
 
             //Test the case
             if(x == 0 || y == 0 || (x < 3 && y < 3)){
+                Assert.assertNull(RULE.checkRuleAt(transition, cell));
+            } else {
+                Assert.assertNotNull(RULE.checkRuleAt(transition, cell));
+            }
+            cell.setData(0);
+        }
+        //Import board and create transition
+        TestUtilities.importTestBoard("puzzles/sudoku/rules/RepeatedNumberContradictionRule/BlankBoard4", sudoku);
+        rootNode = sudoku.getTree().getRootNode();
+        transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        //Loop through every cell
+        for(int i = 0; i < 81; i++){
+            //Reset board
+            SudokuBoard board = (SudokuBoard) transition.getBoard();
+            //Set cell
+            int x = i / 9;
+            int y = i % 9;
+            if((x == 3 && y == 0) || (x == 6 && y == 8)) {
+                continue;
+            }
+            SudokuCell cell = board.getCell(x, y);
+            cell.setData(4);
+
+            //Test the case
+            if((x == 3 || y == 0 || x == 6 || y == 8) || (x > 2 && x < 6 && y < 3) || (x > 5 && y > 5)){
                 Assert.assertNull(RULE.checkRuleAt(transition, cell));
             } else {
                 Assert.assertNotNull(RULE.checkRuleAt(transition, cell));
