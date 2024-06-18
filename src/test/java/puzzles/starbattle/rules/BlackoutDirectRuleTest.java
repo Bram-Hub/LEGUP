02bf6f5f863180eb0519e1a2c46d4b50a85191fd
offline171 <146153141+offline171@users.noreply.github.com>
2024-06-18 15:15:46 -0400

    Blackout Test Suite first test
    First test for Blackout Direct Rule passes

--- src/test/java/puzzles/starbattle/rules/BlackoutDirectRuleTest.java
+++ src/test/java/puzzles/starbattle/rules/BlackoutDirectRuleTest.java
@@ -36,25 +36,39 @@ public class BlackoutDirectRuleTest {
         transition.setRule(RULE);
 
         StarBattleBoard board = (StarBattleBoard) transition.getBoard();
-        StarBattleCell cell1 = board.getCell(0,0); /*
-        StarBattleCell cell2 = board.getCell(0,1);
-        StarBattleCell cell3 = board.getCell(1,0);
-        StarBattleCell cell4 = board.getCell(1,1); */
+        StarBattleCell cell1 = board.getCell(1,0);
+        cell1.setData(StarBattleCellType.BLACK.value);
+        StarBattleCell cell2 = board.getCell(2,0);
+        cell2.setData(StarBattleCellType.BLACK.value);
+        StarBattleCell cell3 = board.getCell(3,0);
+        cell3.setData(StarBattleCellType.BLACK.value);
+        StarBattleCell cell4 = board.getCell(0,1);
+        cell4.setData(StarBattleCellType.BLACK.value);
+        StarBattleCell cell5 = board.getCell(0,2);
+        cell5.setData(StarBattleCellType.BLACK.value);
+        StarBattleCell cell6 = board.getCell(0,3);
+        cell6.setData(StarBattleCellType.BLACK.value);
+        
+        board.addModifiedData(cell1);
+        board.addModifiedData(cell2);
+        board.addModifiedData(cell3);
+        board.addModifiedData(cell4);
+        board.addModifiedData(cell5);
+        board.addModifiedData(cell6);
 
         Assert.assertNull(RULE.checkRule(transition));
 
         for (int i = 0; i < board.getHeight(); ++i) {
             for (int j = 0; j < board.getWidth(); ++j) {
                 Point point = new Point(j,i);
-                if (point.equals(cell1.getLocation()) /*|| point.equals(cell2.getLocation()) ||
-                        point.equals(cell3.getLocation()) || point.equals(cell4.getLocation()) */) {
+                if (point.equals(cell1.getLocation()) || point.equals(cell2.getLocation()) ||
+                        point.equals(cell3.getLocation()) || point.equals(cell4.getLocation()) ||
+                        point.equals(cell5.getLocation()) || point.equals(cell6.getLocation())) {
                     Assert.assertNull(RULE.checkRuleAt(transition, board.getCell(j, i)));
                 }
-                /*
                 else {
                     Assert.assertNotNull(RULE.checkRuleAt(transition, board.getCell(j, i)));
                 }
-                */
             }
         }
     }
