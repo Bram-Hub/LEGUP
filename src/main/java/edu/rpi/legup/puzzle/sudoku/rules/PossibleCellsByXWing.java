d26a1bec3d9966809b13a265703d9739d6e76927
tdou25 <Thomasyd1120@gmail.com>
2024-06-26 21:01:47 -0400

    began work on sudoku puzzle maker

---
+++ src/main/java/edu/rpi/legup/puzzle/sudoku/rules/PossibleCellsByXWing.java
@@ -0,0 +1,107 @@
+package edu.rpi.legup.puzzle.sudoku.rules;
+
+import edu.rpi.legup.model.gameboard.Board;
+import edu.rpi.legup.model.gameboard.CaseBoard;
+import edu.rpi.legup.model.gameboard.PuzzleElement;
+import edu.rpi.legup.model.rules.CaseRule;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.sudoku.GroupType;
+import edu.rpi.legup.puzzle.sudoku.ModelSudokuBoard;
+import edu.rpi.legup.puzzle.sudoku.SudokuBoard;
+import edu.rpi.legup.puzzle.sudoku.SudokuCell;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class PossibleCellsByXWing extends CaseRule{
+
+    // Board math for translating indexes to numbers
+    private ModelSudokuBoard model = new ModelSudokuBoard();
+
+    // Old board for caseBoard reference
+    private SudokuBoard lagBoard;
+
+    public PossibleCellsByXWing(){
+        super("SUDO-CASE-0005",
+                "Possible X-Wing Cells.",
+                "If in two rows and two columns, four cells exist that will allow for the same number such that they form a rectangle," +
+                        " that number must ocupy either one of the vertices of the rectangle.",
+                "edu/rpi/legup/images/sudoku/PossibleValues.png");
+    }
+
+    /**
+     * Checks whether the transition logically follows from the parent node using this rule
+     *
+     * @param transition transition to check
+     * @return null if the child node logically follow from the parent node, otherwise error message
+     */
+    @Override
+    public String checkRuleRaw(TreeTransition transition) {
+        return null;
+    }
+
+    /**
+     * Checks whether the child node logically follows from the parent node at the specific
+     * puzzleElement index using this rule
+     *
+     * @param transition transition to check
+     * @param puzzleElement equivalent puzzleElement
+     * @return null if the child node logically follow from the parent node at the specified
+     *     puzzleElement, otherwise error message
+     */
+    @Override
+    public String checkRuleRawAt(TreeTransition transition, PuzzleElement puzzleElement) {
+        return null;
+    }
+
+    @Override
+    public CaseBoard getCaseBoard(Board board) {
+        SudokuBoard sudokuBoard = (SudokuBoard) board.copy();
+        lagBoard = (SudokuBoard) sudokuBoard.copy();
+        CaseBoard caseBoard = new CaseBoard(sudokuBoard, this);
+        for (PuzzleElement puzzleElement : sudokuBoard.getPuzzleElements()) {
+            puzzleElement.setData(model.getModelRegionNumbers(puzzleElement.getIndex()));
+            caseBoard.addPickableElement(puzzleElement);
+        }
+        return caseBoard;
+    }
+
+    /**
+     * Gets the possible cases at a specific location based on this case rule
+     *
+     * @param board the current board state
+     * @param puzzleElement equivalent puzzleElement
+     * @return a list of elements the specified could be
+     */
+    @Override
+    public ArrayList<Board> getCases(Board board, PuzzleElement puzzleElement) {
+        return getCases(board, puzzleElement, 1, GroupType.REGION);
+    }
+
+    /**
+     * Gets the possible cases at a specific location based on this case rule
+     *
+     * @param board the current board state
+     * @param puzzleElement equivalent puzzleElement
+     * @param value value that the rule will be applied from
+     * @param groupType group type
+     * @return a list of elements the specified could be
+     */
+    public ArrayList<Board> getCases(
+            Board board, PuzzleElement puzzleElement, int value, GroupType groupType) {
+        ArrayList<Board> cases = new ArrayList<>();
+        SudokuBoard sudokuBoard = lagBoard;
+        SudokuCell sourceCell = (SudokuCell) puzzleElement;
+
+        Set<SudokuCell> group = sudokuBoard.getRegion(sourceCell.getGroupIndex());
+        for (SudokuCell cell : group) {
+            if (cell.getData() == 0) {
+                Board newCase = sudokuBoard.copy();
+                PuzzleElement element = newCase.getPuzzleElement(cell);
+                element.setData(model.getModelRegionNumbers(sourceCell.getIndex()));
+                newCase.addModifiedData(element);
+                cases.add(newCase);
+            }
+        }
+        return cases;
+    }
+}
