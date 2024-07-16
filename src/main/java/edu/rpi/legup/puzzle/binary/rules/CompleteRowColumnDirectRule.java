621b5812ab39a275a32a9f25d479b2229cdf923d
Zachary Bonagura <zachbonagura@gmail.com>
2024-07-16 12:27:29 -0400

    Updated binary contradiction images

--- src/main/java/edu/rpi/legup/puzzle/binary/rules/CompleteRowColumnDirectRule.java
+++ src/main/java/edu/rpi/legup/puzzle/binary/rules/CompleteRowColumnDirectRule.java
@@ -18,7 +18,7 @@ public class CompleteRowColumnDirectRule extends DirectRule {
     public CompleteRowColumnDirectRule() {
         super(
                 "BINA-BASC-0003",
-                "Complete Row Column",
+                "Complete Row/Column",
                 "If a row/column of length n contains n/2 of a single value, the remaining cells must contain the other value",
                 "edu/rpi/legup/images/binary/rules/CompleteRowColumnDirectRule.png");
     }
--- src/main/java/edu/rpi/legup/puzzle/binary/rules/EliminateTheImpossibleDirectRule.java
+++ src/main/java/edu/rpi/legup/puzzle/binary/rules/EliminateTheImpossibleDirectRule.java
@@ -22,7 +22,7 @@ public class EliminateTheImpossibleDirectRule extends DirectRule {
         super(
                 "BINA-BASC-0004",
                 "Eliminate The Impossible",
-                "If three adjacent empty cells are open, prevents a trio of numbers to exist",
+                "Out of the remaining empty cells in this row or column, this digit must go here, otherwise there will be a future contradiction",
                 "edu/rpi/legup/images/binary/rules/EliminateTheImpossibleDirectRule.png");
     }
 
