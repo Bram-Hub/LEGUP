72cfdad239e3e79e4c8896512beb1d1315f5e5b5
Thomas Dou <thomasyd1120@gmail.com>
2024-07-19 19:31:39 -0400

    made skeleton implementation of LCFNDR tests

---
+++ src/test/java/puzzles/sudoku/rules/LastCellForNumberDirectRuleRegionTest.java
@@ -0,0 +1,26 @@
+package puzzles.sudoku.rules;
+
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.sudoku.Sudoku;
+import edu.rpi.legup.puzzle.sudoku.SudokuBoard;
+import edu.rpi.legup.puzzle.sudoku.SudokuCell;
+import edu.rpi.legup.puzzle.sudoku.rules.LastCellForNumberDirectRule;
+import edu.rpi.legup.save.InvalidFileFormatException;
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LastCellForNumberDirectRuleRegionTest {
+    private static final LastCellForNumberDirectRule RULE = new LastCellForNumberDirectRule();
+    private static Sudoku sudoku;
+
+    @BeforeClass
+    public static void setUp() {
+        MockGameBoardFacade.getInstance();
+        sudoku = new Sudoku();
+    }
+
+}
