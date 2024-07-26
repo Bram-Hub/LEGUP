c1510b7e8d8ee628563234dd9fac082d86c968a3
summerhenson <summerh@gmail.com>
2024-07-26 14:04:33 -0400

    New 5x5 1 star puzzle file

---
+++ src/test/java/puzzles/starbattle/rules/RegionsWithinRowsDirectRuleTest.java
@@ -0,0 +1,30 @@
+package puzzles.starbattle.rules;
+
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.starbattle.StarBattle;
+import edu.rpi.legup.puzzle.starbattle.StarBattleBoard;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCell;
+import edu.rpi.legup.puzzle.starbattle.StarBattleCellType;
+import edu.rpi.legup.puzzle.starbattle.rules.RegionsWithinRowsDirectRule;
+import edu.rpi.legup.save.InvalidFileFormatException;
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.awt.*;
+
+public class RegionsWithinRowsDirectRuleTest {
+
+    private static final RegionsWithinRowsDirectRule RULE = new RegionsWithinRowsDirectRule();
+    private static StarBattle starbattle;
+
+    @BeforeClass
+    public static void setUp() {
+        MockGameBoardFacade.getInstance();
+        starbattle = new StarBattle();
+    }
+
+}
