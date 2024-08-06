e5d7ded07cb309b28cea9d1add5bcb53dc74982a
offline171 <146153141+offline171@users.noreply.github.com>
2024-08-06 15:07:45 -0400

    Adding border functionality
    Starter code for borders

--- src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleView.java
+++ src/main/java/edu/rpi/legup/puzzle/starbattle/StarBattleView.java
@@ -1,14 +1,19 @@
 package edu.rpi.legup.puzzle.starbattle;
 
 import edu.rpi.legup.controller.BoardController;
+import edu.rpi.legup.model.elements.Element;
 import edu.rpi.legup.model.gameboard.PuzzleElement;
+import edu.rpi.legup.ui.boardview.ElementView;
 import edu.rpi.legup.ui.boardview.GridBoardView;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class StarBattleView extends GridBoardView {
     static Image STAR;
+    private ArrayList<ArrayList<Boolean>> horizontalBorders;    //board.size * board.size+1     left-right up-down
+    private ArrayList<ArrayList<Boolean>> verticalBorders;      //board.size+1 * board.size     left-right up-down
 
     static {
         try {
@@ -23,6 +28,8 @@ public class StarBattleView extends GridBoardView {
 
     public StarBattleView(StarBattleBoard board) {
         super(new BoardController(), new StarBattleController(), board.getDimension());
+        this.horizontalBorders = new ArrayList<>();
+        this.verticalBorders = new ArrayList<>();
 
         for (PuzzleElement puzzleElement : board.getPuzzleElements()) {
             StarBattleCell cell = (StarBattleCell) puzzleElement;
@@ -33,6 +40,16 @@ public class StarBattleView extends GridBoardView {
             elementView.setLocation(
                     new Point(loc.x * elementSize.width, loc.y * elementSize.height));
             elementViews.add(elementView);
+        }
+
+        //initialize horizontal borders
+        for(int i = 0; i < board.getWidth(); i++){
+            ArrayList<Boolean> temp = new ArrayList<>();
+            for(int j = 0; j < board.getHeight() + 1; j++){
+                if(j == 0 || j == board.getHeight()){       //set borders at the ends of the board
+                    temp.add(Boolean.TRUE);
+                }
+            }
         }
     }
 }
