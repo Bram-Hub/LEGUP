2f68c6fc8a3e98de89c211d5dec85e26594d3f4b
ContemporaryNietzsche <jose637007@gmail.com>
2024-08-09 23:58:07 -0400

    added to itp

--- src/main/java/edu/rpi/legup/puzzle/binary/rules/ImplementThePossibleDirectRule.java
+++ src/main/java/edu/rpi/legup/puzzle/binary/rules/ImplementThePossibleDirectRule.java
@@ -9,10 +9,6 @@ import edu.rpi.legup.puzzle.binary.BinaryBoard;
 import edu.rpi.legup.puzzle.binary.BinaryCell;
 import edu.rpi.legup.puzzle.binary.BinaryType;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.lang.Math.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ImplementThePossibleDirectRule extends DirectRule {
@@ -25,9 +21,62 @@ public class ImplementThePossibleDirectRule extends DirectRule {
                 "edu/rpi/legup/images/binary/rules/OneTileGapDirectRule.png");
     }
 
+    /**
+     * Checks if placing a digit in the given cell would create an invalid
+     * configuration with three consecutive identical digits.
+     *
+     * @param origBoard  The original board state.
+     * @param binaryCell The cell to check.
+     * @return True if placing the digit would prevent an invalid trio, false otherwise.
+     */
+    private boolean wouldPreventInvalidTrio(BinaryBoard origBoard, BinaryCell binaryCell) {
+        ArrayList<BinaryCell> row = origBoard.listRowCells(binaryCell.getLocation().y);
+        ArrayList<BinaryCell> col = origBoard.listColCells(binaryCell.getLocation().x);
+
+        return wouldCreateInvalidTrio(row, binaryCell.getLocation().x, binaryCell.getData()) ||
+               wouldCreateInvalidTrio(col, binaryCell.getLocation().y, binaryCell.getData());
+    }
+
+    /**
+     * Checks if placing a digit at the specified position in a row or column would
+     * create an invalid trio of consecutive identical digits.
+     *
+     * @param line     The list of cells in the row or column.
+     * @param pos      The position where the digit is to be placed.
+     * @param digit    The digit to be placed.
+     * @return True if placing the digit would create an invalid trio, false otherwise.
+     */
+    private boolean wouldCreateInvalidTrio(ArrayList<BinaryCell> line, int pos, int digit) {
+        if (pos > 1 && line.get(pos - 1).getData() == digit && line.get(pos - 2).getData() == digit) {
+            return true;
+        }
+        if (pos < line.size() - 2 && line.get(pos + 1).getData() == digit && line.get(pos + 2).getData() == digit) {
+            return true;
+        }
+        if (pos > 0 && pos < line.size() - 1 &&
+            line.get(pos - 1).getData() == digit && line.get(pos + 1).getData() == digit) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks the rule for the given tree transition and puzzle element.
+     *
+     * @param transition    The current tree transition.
+     * @param puzzleElement The puzzle element to check.
+     * @return A message if the rule is invalid, null otherwise.
+     */
     @Override
     public String checkRuleRawAt(TreeTransition transition, PuzzleElement puzzleElement) {
-        return "0s";
+        BinaryBoard origBoard = (BinaryBoard) transition.getParents().get(0).getBoard();
+        BinaryCell binaryCell = (BinaryCell) puzzleElement;
+
+        if (wouldPreventInvalidTrio(origBoard, binaryCell)) {
+            return null;
+        }
+
+        return "Placing this digit would create an invalid trio of consecutive identical digits.";
     }
 
     @Override
