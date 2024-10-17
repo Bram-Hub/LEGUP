ecadb3e17b93befb826aee7e0dfbd762ba545296
pitbull51067 <pitbull51067@yahoo.com>
2024-10-17 14:50:00 -0400

    Touching Sides Direct Rule Startup. Will finish on weekend.

--- src/test/java/puzzles/fillapix/rules/TouchingSidesDirectRuleTest.java
+++ src/test/java/puzzles/fillapix/rules/TouchingSidesDirectRuleTest.java
@@ -1,2 +1,26 @@
-package puzzles.fillapix.rules;public class TouchingSidesDirectRuleTest {
+package puzzles.fillapix.rules;
+
+import edu.rpi.legup.model.tree.TreeNode;
+import edu.rpi.legup.model.tree.TreeTransition;
+import edu.rpi.legup.puzzle.fillapix.Fillapix;
+import edu.rpi.legup.puzzle.fillapix.FillapixBoard;
+import edu.rpi.legup.puzzle.fillapix.rules.TouchingSidesDirectRule;
+import edu.rpi.legup.puzzle.lightup.LightUp;
+import edu.rpi.legup.save.InvalidFileFormatException;
+import java.awt.*;
+import legup.MockGameBoardFacade;
+import legup.TestUtilities;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TouchingSidesDirectRuleTest {
+    private static final TouchingSidesDirectRule RULE =
+            new TouchingSidesDirectRule();
+    private static Fillapix fillapix;
+
+    @BeforeClass
+    public static void setUp() {
+        fillapix = new Fillapix();
+    }
 }
