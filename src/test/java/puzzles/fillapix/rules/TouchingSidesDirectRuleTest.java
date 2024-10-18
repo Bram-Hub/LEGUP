c4fd39a4ba743cd01f34be3f19704fee890ae9b5
pitbull51067 <pitbull51067@yahoo.com>
2024-10-18 17:34:08 -0400

    Made a working test file, will edit it later. Made some changes to the rule test code.

--- src/test/java/puzzles/fillapix/rules/TouchingSidesDirectRuleTest.java
+++ src/test/java/puzzles/fillapix/rules/TouchingSidesDirectRuleTest.java
@@ -6,6 +6,9 @@ import edu.rpi.legup.puzzle.fillapix.Fillapix;
 import edu.rpi.legup.puzzle.fillapix.FillapixBoard;
 import edu.rpi.legup.puzzle.fillapix.rules.TouchingSidesDirectRule;
 import edu.rpi.legup.puzzle.fillapix.Fillapix;
+import edu.rpi.legup.puzzle.lightup.LightUpBoard;
+import edu.rpi.legup.puzzle.lightup.LightUpCell;
+import edu.rpi.legup.puzzle.lightup.LightUpCellType;
 import edu.rpi.legup.save.InvalidFileFormatException;
 import java.awt.*;
 import legup.MockGameBoardFacade;
@@ -22,5 +25,40 @@ public class TouchingSidesDirectRuleTest {
     @BeforeClass
     public static void setUp() {
         fillapix = new Fillapix();
+    }
+
+    @Test
+    public void TouchingSidesTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard(
+                "puzzles/fillapix/rules/TouchingSidesDirectRule/FinishWithBulbs", fillapix);
+        TreeNode rootNode = fillapix.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        // get board state
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+
+        // change the board's cells considering the FinishWithBulbs rule to empty
+        LightUpCell cell1 = board.getCell(1, 0);
+        cell1.setData(LightUpCellType.BULB.value);
+        board.addModifiedData(cell1);
+
+        // confirm there is a logical following of the FinishWithBulbs rule
+        Assert.assertNull(RULE.checkRule(transition));
+
+        // check every square except the top center (2,0)
+        LightUpCell c;
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int j = 0; j < board.getWidth(); j++) {
+                c = board.getCell(j, i);
+                if (i == 0 && j == 1) {
+                    // logically follows
+                    Assert.assertNull(RULE.checkRuleAt(transition, c));
+                } else {
+                    // does not use the rule to logically follow
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, c));
+                }
+            }
+        }
     }
 }
