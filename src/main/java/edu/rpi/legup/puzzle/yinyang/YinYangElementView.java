082cfa46c23563793e69bb760846583790b58107
sdhoat <121777913+sdhoat@users.noreply.github.com>
2024-11-22 18:00:42 -0500

    Circle creator for each cell in tile
    Co-Authored-By: andrewtarnavsky <145161755+andrewtarnavsky@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangElementView.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangElementView.java
@@ -1,4 +1,24 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-public class YinYangElementView {
+import edu.rpi.legup.model.gameboard.PuzzleElementView;
+import java.awt.*;
+
+public class YinYangElementView extends PuzzleElementView {
+    public YinYangElementView(YinYangCell cell) {
+        super(cell);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        YinYangCell cell = (YinYangCell) getElement();
+
+        if (cell.getType() == YinYangType.WHITE) {
+            g.setColor(Color.WHITE);
+            g.fillOval(0, 0, getWidth(), getHeight());
+        } else if (cell.getType() == YinYangType.BLACK) {
+            g.setColor(Color.BLACK);
+            g.fillOval(0, 0, getWidth(), getHeight());
+        }
+    }
 }
