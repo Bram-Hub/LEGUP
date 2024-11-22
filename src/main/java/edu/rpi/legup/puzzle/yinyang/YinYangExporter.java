dbf1ccc97ca491c601d8346984f92e856edef585
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-11-22 18:02:34 -0500

    Implemented exporter
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangExporter.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangExporter.java
@@ -1,4 +1,30 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-public class YinYangExporter {
+import edu.rpi.legup.model.PuzzleExporter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class YinYangExporter extends PuzzleExporter {
+    public YinYangExporter(YinYang puzzle) {
+        super(puzzle);
+    }
+
+    @Override
+    public Element exportBoard(Document document) {
+        YinYangBoard board = (YinYangBoard) puzzle.getBoardView().getBoard();
+        Element boardElement = document.createElement("board");
+        boardElement.setAttribute("width", String.valueOf(board.getWidth()));
+        boardElement.setAttribute("height", String.valueOf(board.getHeight()));
+
+        for (PuzzleElement element : board.getPuzzleElements()) {
+            YinYangCell cell = (YinYangCell) element;
+            Element cellElement = document.createElement("cell");
+            cellElement.setAttribute("x", String.valueOf(cell.getX()));
+            cellElement.setAttribute("y", String.valueOf(cell.getY()));
+            cellElement.setAttribute("type", cell.getType().toString());
+            boardElement.appendChild(cellElement);
+        }
+
+        return boardElement;
+    }
 }
