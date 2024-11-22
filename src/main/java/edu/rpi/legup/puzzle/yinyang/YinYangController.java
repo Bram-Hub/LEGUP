3f2028249c1aae50e66c64d343566e8b583aa846
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-11-22 17:59:22 -0500

    Implemented controller
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangController.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangController.java
@@ -1,4 +1,24 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-public class YinYangController {
+import edu.rpi.legup.controller.BoardController;
+
+public class YinYangController extends BoardController {
+
+    @Override
+    public boolean validateBoard() {
+        YinYangBoard board = (YinYangBoard) getPuzzle().getCurrentBoard();
+
+        // Validate the puzzle rules
+        if (!YinYangUtilities.validateNo2x2Blocks(board)) {
+            System.out.println("Invalid board: 2x2 blocks detected!");
+            return false;
+        }
+
+        if (!YinYangUtilities.validateConnectivity(board)) {
+            System.out.println("Invalid board: disconnected groups detected!");
+            return false;
+        }
+
+        return true;
+    }
 }
