f47478be108c77d790f3819c052328f10e814aa2
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-11-22 18:02:04 -0500

    Implemented importer
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangImporter.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangImporter.java
@@ -1,4 +1,31 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-public class YinYangImporter {
+import edu.rpi.legup.model.PuzzleImporter;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class YinYangImporter extends PuzzleImporter {
+    public YinYangImporter(YinYang puzzle) {
+        super(puzzle);
+    }
+
+    @Override
+    public void initializeBoard(Element boardElement) {
+        int width = Integer.parseInt(boardElement.getAttribute("width"));
+        int height = Integer.parseInt(boardElement.getAttribute("height"));
+        YinYangBoard board = new YinYangBoard(width, height);
+
+        NodeList cellNodes = boardElement.getElementsByTagName("cell");
+        for (int i = 0; i < cellNodes.getLength(); i++) {
+            Element cellElement = (Element) cellNodes.item(i);
+            int x = Integer.parseInt(cellElement.getAttribute("x"));
+            int y = Integer.parseInt(cellElement.getAttribute("y"));
+            YinYangType type = YinYangType.valueOf(cellElement.getAttribute("type"));
+
+            YinYangCell cell = board.getCell(x, y);
+            cell.setType(type);
+        }
+
+        puzzle.setCurrentBoard(board);
+    }
 }
