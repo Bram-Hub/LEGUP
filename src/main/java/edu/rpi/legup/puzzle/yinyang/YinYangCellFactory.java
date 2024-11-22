a9f3b12f86c70c1817708368a3ea5281971eaeff
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-11-22 17:50:06 -0500

    Revert "Implemented basic version of CellFactory"
    This reverts commit fa3a0dc2561c5048e49388a33631c7170788d816.

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangCellFactory.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangCellFactory.java
@@ -1,13 +1,4 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-import edu.rpi.legup.model.gameboard.PuzzleCellFactory;
-import edu.rpi.legup.model.gameboard.PuzzleElement;
-
-public class YinYangCellFactory extends PuzzleCellFactory {
-    @Override
-    public PuzzleElement createCell(int data, Object... params) {
-        int x = (int) params[0];
-        int y = (int) params[1];
-        return new YinYangCell(data, x, y);
-    }
+public class YinYangCellFactory {
 }
