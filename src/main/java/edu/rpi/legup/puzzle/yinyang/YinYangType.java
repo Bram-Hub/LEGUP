c75ad2bdeda28625d473c79f476a66ddf2b92d65
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-12-06 18:06:45 -0500

    Revert "Added complex utility methods (enhanced validation logic) and implemented YinYang Type to fix errors"
    This reverts commit d6a8a2eee5d55823301288b1090c1b9920ce678f.

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangType.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangType.java
@@ -1,16 +1,4 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-public enum YinYangType {
-    UNKNOWN, // Represents an uninitialized or empty cell
-    WHITE,   // Represents a WHITE cell
-    BLACK;   // Represents a BLACK cell
-
-    /**
-     * Converts the enum value to an integer representation.
-     *
-     * @return the ordinal value adjusted for Yin Yang data
-     */
-    public int toValue() {
-        return this.ordinal();
-    }
+public class YinYangType {
 }
