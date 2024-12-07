1201b26a14f0418cd8c2676f19ded7982d8211fc
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-12-06 19:09:54 -0500

    Fixed bugs in YinYangCell file
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangCell.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangCell.java
@@ -1,31 +1,38 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-import edu.rpi.legup.model.gameboard.PuzzleElement;
+import edu.rpi.legup.model.gameboard.GridCell;
 
-public class YinYangCell extends PuzzleElement {
-    private YinYangType type;
-    private int x, y;
+public class YinYangCell extends GridCell<YinYangType> {
 
-    public YinYangCell(int data, int x, int y) {
-        super(data, null);
-        this.x = x;
-        this.y = y;
-        this.type = YinYangType.UNKNOWN;
+    public YinYangCell(YinYangType type, int x, int y) {
+        super(type, x, y); // Use YinYangType directly
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
+    /**
+     * Gets the type of the cell.
+     *
+     * @return the YinYangType of the cell
+     */
     public YinYangType getType() {
-        return type;
+        return getData();
     }
 
+    /**
+     * Sets the type of the cell.
+     *
+     * @param type the YinYangType to set
+     */
     public void setType(YinYangType type) {
-        this.type = type;
+        setData(type);
+    }
+
+    /**
+     * Performs a deep copy of the YinYangCell.
+     *
+     * @return a new copy of the YinYangCell
+     */
+    @Override
+    public YinYangCell copy() {
+        return new YinYangCell(getType(), location.x, location.y);
     }
 }
