b5c4374ef7d267b00c46ecdfd7d3b7e0d2ec80e7
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-12-06 20:01:07 -0500

    Implemented new functions in Importer class and fixed bugs
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangImporter.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangImporter.java
@@ -1,31 +1,82 @@
 package edu.rpi.legup.puzzle.yinyang;
 
 import edu.rpi.legup.model.PuzzleImporter;
+import edu.rpi.legup.save.InvalidFileFormatException;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class YinYangImporter extends PuzzleImporter {
+
     public YinYangImporter(YinYang puzzle) {
         super(puzzle);
     }
 
     @Override
-    public void initializeBoard(Element boardElement) {
+    public boolean acceptsRowsAndColumnsInput() {
+        return true;
+    }
+
+    @Override
+    public boolean acceptsTextInput() {
+        return false;
+    }
+
+    @Override
+    public void initializeBoard(int rows, int columns) {
+        YinYangBoard board = new YinYangBoard(columns, rows);
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                YinYangCell cell = new YinYangCell(YinYangType.UNKNOWN, x, y);
+                board.setCell(x, y, cell);
+            }
+        }
+
+        puzzle.setCurrentBoard(board);
+    }
+
+    @Override
+    public void initializeBoard(Node node) throws InvalidFileFormatException {
+        if (!node.getNodeName().equalsIgnoreCase("board")) {
+            throw new InvalidFileFormatException("YinYang Importer: cannot find board element");
+        }
+
+        Element boardElement = (Element) node;
+        if (boardElement.getElementsByTagName("cells").getLength() == 0) {
+            throw new InvalidFileFormatException("YinYang Importer: no elements found for board");
+        }
+
+        Element cellsElement = (Element) boardElement.getElementsByTagName("cells").item(0);
+        NodeList cellNodes = cellsElement.getElementsByTagName("cell");
+
         int width = Integer.parseInt(boardElement.getAttribute("width"));
         int height = Integer.parseInt(boardElement.getAttribute("height"));
         YinYangBoard board = new YinYangBoard(width, height);
 
-        NodeList cellNodes = boardElement.getElementsByTagName("cell");
         for (int i = 0; i < cellNodes.getLength(); i++) {
-            Element cellElement = (Element) cellNodes.item(i);
-            int x = Integer.parseInt(cellElement.getAttribute("x"));
-            int y = Integer.parseInt(cellElement.getAttribute("y"));
-            YinYangType type = YinYangType.valueOf(cellElement.getAttribute("type"));
+            YinYangCell cell = (YinYangCell) puzzle.getFactory().importCell(cellNodes.item(i), board);
+            if (cell.getType() != YinYangType.UNKNOWN) {
+                cell.setModifiable(false);
+                cell.setGiven(true);
+            }
+            board.setCell(cell.getX(), cell.getY(), cell);
+        }
 
-            YinYangCell cell = board.getCell(x, y);
-            cell.setType(type);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (board.getCell(x, y) == null) {
+                    YinYangCell cell = new YinYangCell(YinYangType.UNKNOWN, x, y);
+                    board.setCell(x, y, cell);
+                }
+            }
         }
 
         puzzle.setCurrentBoard(board);
+    }
+
+    @Override
+    public void initializeBoard(String[] statements) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("YinYang does not support text input");
     }
 }
