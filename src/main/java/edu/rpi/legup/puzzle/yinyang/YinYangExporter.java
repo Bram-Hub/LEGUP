44405cd176398f1c63c36226d546edf376fa7b0b
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-12-06 19:07:53 -0500

    Fixed Exporter
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangExporter.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangExporter.java
@@ -1,30 +1,44 @@
 package edu.rpi.legup.puzzle.yinyang;
 
 import edu.rpi.legup.model.PuzzleExporter;
+import edu.rpi.legup.model.gameboard.PuzzleElement;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class YinYangExporter extends PuzzleExporter {
+
     public YinYangExporter(YinYang puzzle) {
         super(puzzle);
     }
 
     @Override
-    public Element exportBoard(Document document) {
-        YinYangBoard board = (YinYangBoard) puzzle.getBoardView().getBoard();
+    protected Element createBoardElement(Document document) {
+        YinYangBoard board;
+
+        // Check whether the tree or the board view should be used
+        if (puzzle.getTree() != null) {
+            board = (YinYangBoard) puzzle.getTree().getRootNode().getBoard();
+        } else {
+            board = (YinYangBoard) puzzle.getBoardView().getBoard();
+        }
+
         Element boardElement = document.createElement("board");
         boardElement.setAttribute("width", String.valueOf(board.getWidth()));
         boardElement.setAttribute("height", String.valueOf(board.getHeight()));
 
+        // Create the cells element
+        Element cellsElement = document.createElement("cells");
         for (PuzzleElement element : board.getPuzzleElements()) {
             YinYangCell cell = (YinYangCell) element;
-            Element cellElement = document.createElement("cell");
-            cellElement.setAttribute("x", String.valueOf(cell.getX()));
-            cellElement.setAttribute("y", String.valueOf(cell.getY()));
-            cellElement.setAttribute("type", cell.getType().toString());
-            boardElement.appendChild(cellElement);
+
+            // Only export cells that are valid and initialized
+            if (cell.getType() != YinYangType.UNKNOWN) {
+                Element cellElement = puzzle.getFactory().exportCell(document, cell);
+                cellsElement.appendChild(cellElement);
+            }
         }
 
+        boardElement.appendChild(cellsElement);
         return boardElement;
     }
 }
