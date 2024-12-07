70485319b4dfa2fd6d40767cf0af56e74e9b209f
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-12-06 19:51:17 -0500

    Implemented View Class
    Co-Authored-By: sdhoat <121777913+sdhoat@users.noreply.github.com>

--- src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangView.java
+++ src/main/java/edu/rpi/legup/puzzle/yinyang/YinYangView.java
@@ -1,4 +1,25 @@
 package edu.rpi.legup.puzzle.yinyang;
 
-public class YinYangView {
+import edu.rpi.legup.controller.BoardController;
+import edu.rpi.legup.model.gameboard.PuzzleElement;
+import edu.rpi.legup.ui.boardview.GridBoardView;
+
+import java.awt.*;
+
+public class YinYangView extends GridBoardView {
+
+    public YinYangView(YinYangBoard board) {
+        super(new BoardController(), new YinYangController(), board.getDimension());
+
+        for (PuzzleElement puzzleElement : board.getPuzzleElements()) {
+            YinYangCell cell = (YinYangCell) puzzleElement;
+            Point loc = cell.getLocation();
+            YinYangElementView elementView = new YinYangElementView(cell);
+            elementView.setIndex(cell.getIndex());
+            elementView.setSize(elementSize);
+            elementView.setLocation(
+                    new Point(loc.x * elementSize.width, loc.y * elementSize.height));
+            elementViews.add(elementView);
+        }
+    }
 }
