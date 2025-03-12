f46f05ee71d1cc678b4a1e29a922d2ce053d0658
Jaden Tian <56417002+jadeandtea@users.noreply.github.com>
2025-03-12 00:11:33 -0400

    Goal notes
    Comments describing future plans for Goal object

--- src/main/java/edu/rpi/legup/model/GoalType.java
+++ src/main/java/edu/rpi/legup/model/GoalType.java
@@ -2,6 +2,9 @@ package edu.rpi.legup.model;
 
 public enum GoalType {
     NONE, SETCELL, DETERMINECELL;
+    // Prove cell value as indicated, prove cell value not necessarily as indicated
+    // Figure out cell value must be value, figure out cell can be different values
+    // In future, multiple cell goals?
 
     public int toValue() { return this.ordinal(); }
 }
---
+++ src/main/java/edu/rpi/legup/model/GoalView.java
@@ -0,0 +1,19 @@
+package edu.rpi.legup.model;
+
+public class GoalView {
+    // Temporary Class to document wanted changes
+
+    /**
+     * Puzzle Board:
+     *  Goal should be highlighted (in brown?) at all times
+     *      - Hovering should specify exactly what the goal wants
+     *          like how rule tooltips work
+     *      - Different indicators for the different types of rules?
+     *      - Goal text needs to be dynamically allocated
+     * Puzzle Editor:
+     *  Default 'Goal' element highlight
+     *      - Clicking on same cell cycles through goal types
+     *          - Visual is curved arrow
+     *      - Highlight is independent of cell type
+     */
+}
