839cd95359d1573e1f6007e74fa6e3a577563e7b
pitbull51067 <pitbull51067@yahoo.com>
2025-04-23 21:08:44 -0400

    This doesn't work, confirmed.

--- src/main/java/edu/rpi/legup/ui/ScrollView.java
+++ src/main/java/edu/rpi/legup/ui/ScrollView.java
@@ -16,7 +16,7 @@ public class ScrollView extends JScrollPane {
 
     private static final double minScale = 0.25;
     private static final double maxScale = 4.0;
-    private static final double[] levels = {0.25, 1.0 / 3.0, 0.50, 2.0 / 3.0, 1.0, 2.0, 3.0, 4.0};
+    private static final double[] levels = {0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.75, 1.0, 1.25, 1.5, 2.0, 3.0, 4.0};
 
     private Dimension viewSize;
     private Dimension zoomSize;
